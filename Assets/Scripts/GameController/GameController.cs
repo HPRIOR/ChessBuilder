c1dc5bb1f5326677f71714dee7104cf250aef7a8
Harry Prior <harryprior@hotmail.co.uk>
2021-03-06 16:11:28 +0000

    Add mechanism for changing turns

--- Assets/Scripts/GameController/GameController.cs
+++ Assets/Scripts/GameController/GameController.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-   
+    public PieceColour Turn { get; private set; } = PieceColour.White;
+
+    public void ChangeTurn() =>
+        _ = Turn == PieceColour.White ? Turn = PieceColour.Black : PieceColour.White;
+    
     
 }

8d0ca367523803222115a6218135aa1b67e4a583
Harry Prior <harryprior@hotmail.co.uk>
2021-03-06 17:00:19 +0000

    Add class to handle logic for allowing piece movement

---
+++ Assets/Scripts/Pieces/Implementations/PieceMover.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceMover : MonoBehaviour
+{
+    private GameController _gameController;
+    private BoardController _boardController;
+    private HashSet<PieceType> _blackPieces = new HashSet<PieceType>() 
+    {
+        PieceType.BlackBishop, 
+        PieceType.BlackKing,
+        PieceType.BlackPawn,
+        PieceType.BlackQueen,
+        PieceType.BlackRook,
+        PieceType.BlackKnight
+    };
+    private HashSet<PieceType> _whitePieces = new HashSet<PieceType>() 
+    {
+        PieceType.WhiteBishop, 
+        PieceType.WhiteKing,
+        PieceType.WhitePawn,
+        PieceType.WhiteQueen,
+        PieceType.WhiteRook,
+        PieceType.WhiteKnight
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameController = GetComponent<GameController>();
+        _boardController = GetComponent<BoardController>();
+        
+    }
+
+    // references _boardController.PossibleMoves + _gameController.Turn
+    private bool CanMove(PieceType pieceType, IBoardPosition destination)
+    {
+        var pieceColoursTurn = 
+            _gameController.Turn == PieceColour.White & _whitePieces.Contains(pieceType)
+            || _gameController.Turn == PieceColour.Black & _blackPieces.Contains(pieceType);
+
+        throw new NotImplementedException();
+    }
+
+
+    // signals to caller that move has taken place (used by drag and drop) while updating board state
+    public bool Move(PieceType piece, IBoardPosition previousBoardPosition, IBoardPosition newBoardPosition)
+    {
+        throw new NotImplementedException();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

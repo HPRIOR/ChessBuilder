7676a3a887f92667d84bf86c6f948cd97b6752fb
Harry Prior <harryprior@hotmail.co.uk>
2021-03-07 10:03:53 +0000

    Add command pattern for handling user input

---
+++ Assets/Scripts/UserInput/CommandInvoker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandInvoker : MonoBehaviour
+{
+    public ICommand currentCommand { get; set; }
+    private IList<ICommand> commandBuffer;
+
+    private void Start()
+    {
+        commandBuffer = new List<ICommand>();
+    }
+    private void Update()
+    {
+        if (currentCommand is not null)
+        {
+            currentCommand.Execute();
+            commandBuffer.Add(currentCommand);
+            currentCommand = null;
+        }
+    }
+
+}

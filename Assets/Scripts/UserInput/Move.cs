caecaf342950fafb827ce45f36b9abe26c8c597c
Harry Prior <harryprior@hotmail.co.uk>
2021-03-08 09:54:35 +0000

    Add class which contians piece move data

---
+++ Assets/Scripts/UserInput/Move.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move 
+{
+    public ITile FromTile { get; private set; }
+    public ITile ToTile { get; private set; }
+
+}

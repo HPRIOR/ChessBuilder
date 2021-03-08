d1a71e049fefd0e96865938c5b7b290c1f241ae1
Harry Prior <harryprior@hotmail.co.uk>
2021-03-08 13:59:20 +0000

    Revert "#"
    This reverts commit ee34b7343d70f66f77911f1a8606e37d156d6e92.

--- Assets/Scripts/UserInput/MovePieceCommand.cs
+++ Assets/Scripts/UserInput/MovePieceCommand.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MovePieceCommand : ICommand
 {
     private static PieceMover _pieceMover;
+    private IPieceInfo pieceInfo;
 
     public MovePieceCommand(GameObject piece, IBoardPosition destination)
     {

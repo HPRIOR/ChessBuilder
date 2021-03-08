da315efc07fe947daed6014c851a931797229682
Harry Prior <harryprior@hotmail.co.uk>
2021-03-08 09:50:42 +0000

    Create Piece class to represent Piece with tile information and monobehaviour

---
+++ Assets/Scripts/Pieces/Implementations/Piece.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour, IPiece
+{
+    public ITile PieceTile { get; set; }
+
+    private void Update()
+    {
+        if (PieceTile.CurrentPiece == PieceType.None)
+            Destroy(gameObject);
+    }
+}

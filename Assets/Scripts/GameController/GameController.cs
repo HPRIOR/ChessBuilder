7fd19fec9914c7dcccb1e15b99e39eced1a3676b
Harry Prior <harryprior@hotmail.co.uk>
2021-03-08 10:51:04 +0000

    Add mechanism for changing turn in the game controller

--- Assets/Scripts/GameController/GameController.cs
+++ Assets/Scripts/GameController/GameController.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+        public PieceColour Turn { get; private set; } = PieceColour.White;
     // will need to reference the board controller do evaluate draw/win 
     // checkmate not check mate 
     public void EvaluateGame()
     {
 
     }
-       
+    public void ChangeTurn() =>
+        _ = Turn == PieceColour.White ? Turn = PieceColour.Black : Turn = PieceColour.White;
+
     
 }

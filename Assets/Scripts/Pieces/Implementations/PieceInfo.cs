3e0f0b37fac329dec7a92728fc799c691c8525be
Harry Prior <harryprior@hotmail.co.uk>
2021-03-08 10:51:53 +0000

    Remove PieceInfo class

--- Assets/Scripts/Pieces/Implementations/PieceInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PieceInfo : MonoBehaviour, IPieceInfo
-{
-    public PieceType PieceType { get; set; }
-    public IBoardPosition boardPosition { get; set;} 
-}

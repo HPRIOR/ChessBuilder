ee34b7343d70f66f77911f1a8606e37d156d6e92
Harry Prior <harryprior@hotmail.co.uk>
2021-03-08 13:58:59 +0000

    #

---
+++ Assets/Scripts/UserInput/MoveData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveData : IMoveData
+{
+    public MoveData(ITile fromTile, ITile toTile)
+    {
+        FromTile = fromTile;
+        ToTile = toTile;
+        DisplacedPiece = ToTile.CurrentPiece;
+        MovedPiece = fromTile.CurrentPiece;
+    }
+    public ITile FromTile { get; private set; }
+    public ITile ToTile { get; private set; }
+    public PieceType DisplacedPiece { get; private set; }
+    public PieceType MovedPiece { get; private set; }
+
+}
--- Assets/Scripts/UserInput/MovePieceCommand.cs
+++ Assets/Scripts/UserInput/MovePieceCommand.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class MovePieceCommand : ICommand
 {
     private static PieceMover _pieceMover;
-    private IPieceInfo pieceInfo;
 
     public MovePieceCommand(GameObject piece, IBoardPosition destination)
     {

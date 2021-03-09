9a97cc3ef95c9dd62a33233a345a50691c25821c
Harry Prior <harryprior@hotmail.co.uk>
2021-03-09 14:00:45 +0000

    Move logic from board controller into game controller

--- Assets/Scripts/GameController/GameController.cs
+++ Assets/Scripts/GameController/GameController.cs
@@ -1,20 +1,49 @@
-﻿using System.Collections;
-using System.Linq;
-using Zenject;
+﻿using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
+using System;
 
 public class GameController : MonoBehaviour
 {
+    IBoardGenerator _boardLogicGenerator;
+    IPieceGenerator _pieceGenerator;
+    public ITile[,] Board { get; private set; }
+    public IDictionary<ITile, IBoardPosition> PossibleMoves { get; private set; }
     public PieceColour Turn { get; private set; } = PieceColour.White;
-    // will need to reference the board controller do evaluate draw/win 
-    // checkmate not check mate 
+    private void Start()
+    {
+        Board = _boardLogicGenerator.GenerateBoard();
+        _pieceGenerator.GeneratePiece(Board[0, 0], PieceType.BlackKing);
+        _pieceGenerator.GeneratePiece(Board[7, 7], PieceType.WhiteKing);
+        EvaluateBoardMoves();
+    }
+
+    [Inject]
+    private void Constructor(IBoardGenerator boardLogicGenerator, IPieceGenerator pieceGenerator)
+    {
+        _boardLogicGenerator = boardLogicGenerator;
+        _pieceGenerator = pieceGenerator;
+    }
+    
+    // mate/check/draw 
     public void EvaluateGame()
     {
 
     }
+     public void EvaluateBoardMoves()
+    {
+        // update possible moves dict
+    }
+
     public void ChangeTurn() =>
         _ = Turn == PieceColour.White ? Turn = PieceColour.Black : Turn = PieceColour.White;
 
     
+    public ITile GetTileAt(IBoardPosition boardPosition) =>
+        Board[boardPosition.X, boardPosition.Y];
+    
+
+   
 }

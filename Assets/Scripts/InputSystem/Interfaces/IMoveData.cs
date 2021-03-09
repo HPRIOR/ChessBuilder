d877582270ce45e432fbf7d587da5fd715e1a84e
Harry Prior <harryprior@hotmail.co.uk>
2021-03-09 14:03:26 +0000

    Add interface for MoveData

---
+++ Assets/Scripts/InputSystem/Interfaces/IMoveData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public interface IMoveData
+{
+    IBoardPosition InitialBoardPosition { get; }
+    IBoardPosition DestinationBoardPosition { get; }
+    GameObject DisplacedPiece { get; }
+    GameObject MovedPiece { get; }
+    IPiece MovedPieceComponent { get; }
+
+    
+}

6e7bcc1e30feb35120cbaa4620ed46e3b786b762
Harry Prior <harryprior@hotmail.co.uk>
2021-03-09 14:02:20 +0000

    Add class for testing input

---
+++ Assets/Scripts/InputSystem/Implementation/InputTester.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputTester : MonoBehaviour
+{
+    CommandInvoker _commandInvoker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _commandInvoker = GetComponent<CommandInvoker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) _commandInvoker.RollBackCommand();
+    }
+}

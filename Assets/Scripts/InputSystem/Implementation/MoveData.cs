04be23a701e41c43fd35012589d7695e3aceea57
Harry Prior <harryprior@hotmail.co.uk>
2021-03-09 14:02:49 +0000

    Add class for generating data for each move

---
+++ Assets/Scripts/InputSystem/Implementation/MoveData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveData : IMoveData
+{
+    private static GameController _gameController = 
+        GameObject
+        .FindGameObjectWithTag("GameController")
+        .GetComponent<GameController>();
+
+    public IBoardPosition InitialBoardPosition { get; }
+    public IBoardPosition DestinationBoardPosition { get; }
+    public GameObject DisplacedPiece { get; }
+    public GameObject MovedPiece { get; }
+    public IPiece MovedPieceComponent {get;}
+    
+    public MoveData(GameObject movedPiece, IBoardPosition destination)
+    {
+        MovedPiece = movedPiece;
+        MovedPieceComponent = movedPiece.GetComponent<Piece>();
+        InitialBoardPosition = MovedPieceComponent.boardPosition;
+
+        DisplacedPiece = _gameController.GetTileAt(destination).CurrentPiece;
+        DestinationBoardPosition = destination;
+
+    }
+}

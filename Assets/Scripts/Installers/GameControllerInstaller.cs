2001a2ff4f1e55be9c91f00e0cb6ac1d1bbf0258
Harry Prior <harryprior@hotmail.co.uk>
2021-03-09 14:04:15 +0000

    Bind IPieceGenerator to PieceGenerator

--- Assets/Scripts/Installers/GameControllerInstaller.cs
+++ Assets/Scripts/Installers/GameControllerInstaller.cs
@@ -9,6 +9,7 @@ public class GameControllerInstaller : MonoInstaller<GameControllerInstaller>
     {
         Container.Bind<IBoardRenderer>().To<BoardRenderer>().AsTransient();
         Container.Bind<IBoardGenerator>().To<BoardInitialiser>().AsTransient();
-        Container.Bind<IPieceGanerator>().To<PieceGenerator>().AsTransient();
+        Container.Bind<IPieceGenerator>().To<PieceGenerator>().AsSingle();
+        
     }
 }

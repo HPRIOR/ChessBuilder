c8d5580b5fcbf02a7676bf7b7ffb7ebffc852980
Harry Prior <harryprior@hotmail.co.uk>
2021-03-13 09:11:10 +0000

    update gamecontroller installer

--- Assets/Scripts/Installers/GameControllerInstaller.cs
+++ Assets/Scripts/Installers/GameControllerInstaller.cs
@@ -5,7 +5,5 @@ public class GameControllerInstaller : MonoInstaller<GameControllerInstaller>
     public override void InstallBindings()
     {
         Container.Bind<IBoardRenderer>().To<BoardRenderer>().AsTransient();
-        Container.Bind<IBoardGenerator>().To<BoardInitialiser>().AsTransient();
-        Container.Bind<PieceInjector>().AsSingle();
     }
 }

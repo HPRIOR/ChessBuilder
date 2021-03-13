209a622e550648b6ba96e872c3009c39e4527ba4
Harry Prior <harryprior@hotmail.co.uk>
2021-03-13 09:11:51 +0000

    Create static class to represent dictionary map of sprite asset locations

--- Assets/Scripts/Pieces/Implementations/Piece.cs
+++ Assets/Scripts/Pieces/Implementations/Piece.cs
@@ -5,22 +5,7 @@ using Zenject;
 
 public class Piece : MonoBehaviour
 {
-    private Dictionary<PieceType, string> spriteAssetMap = new Dictionary<PieceType, string>()
-    {
-        {PieceType.BlackPawn, "Assets/Sprites/ChessPieces/45x45/45px-Chess_pdt45.svg.png"},
-        {PieceType.BlackBishop,"Assets/Sprites/ChessPieces/45x45/45px-Chess_bdt45.svg.png" },
-        {PieceType.BlackKnight, "Assets/Sprites/ChessPieces/45x45/45px-Chess_ndt45.svg.png"},
-        {PieceType.BlackRook, "Assets/Sprites/ChessPieces/45x45/45px-Chess_rdt45.svg.png" },
-        {PieceType.BlackKing, "Assets/Sprites/ChessPieces/45x45/45px-Chess_kdt45.svg.png"},
-        {PieceType.BlackQueen, "Assets/Sprites/ChessPieces/45x45/45px-Chess_qdt45.svg.png"},
-        {PieceType.WhitePawn, "Assets/Sprites/ChessPieces/45x45/45px-Chess_plt45.svg.png"},
-        {PieceType.WhiteBishop, "Assets/Sprites/ChessPieces/45x45/45px-Chess_blt45.svg.png"},
-        {PieceType.WhiteKnight, "Assets/Sprites/ChessPieces/45x45/45px-Chess_nlt45.svg.png"},
-        {PieceType.WhiteRook, "Assets/Sprites/ChessPieces/45x45/45px-Chess_rlt45.svg.png"},
-        {PieceType.WhiteKing, "Assets/Sprites/ChessPieces/45x45/45px-Chess_klt45.svg.png"},
-        {PieceType.WhiteQueen, "Assets/Sprites/ChessPieces/45x45/45px-Chess_qlt45.svg.png"}
-    };
-
+    private static IDictionary<PieceType, string> _spriteAssetMap = PieceSpriteAssetManager.PieceSpriteMap; 
     public IBoardPosition BoardPosition { get; set; }
     public PieceColour PieceColour { get; set; }
     public PieceType PieceType { get; set; }
@@ -32,9 +17,8 @@ public class Piece : MonoBehaviour
     {
         gameObject.transform.parent = GameObject.FindGameObjectWithTag("Pieces").transform;
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        _spriteRenderer.sprite = AssetDatabase.LoadAssetAtPath<Sprite>(spriteAssetMap[PieceType]);
+        _spriteRenderer.sprite = AssetDatabase.LoadAssetAtPath<Sprite>(_spriteAssetMap[PieceType]);
         gameObject.transform.position = BoardPosition.Position;
-
     }
 
     [Inject]
---
+++ Assets/Scripts/Pieces/Implementations/PieceSpriteAssetManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+internal static class PieceSpriteAssetManager {
+    private static  IDictionary<PieceType, string> _pieceSpriteAssetManager = 
+        new Dictionary<PieceType, string>()
+        {
+            {PieceType.BlackPawn, "Assets/Sprites/ChessPieces/240x240/240px-Chess_pdt45.svg.png"},
+            {PieceType.BlackBishop,"Assets/Sprites/ChessPieces/240x240/240px-Chess_bdt45.svg.png" },
+            {PieceType.BlackKnight, "Assets/Sprites/ChessPieces/240x240/240px-Chess_ndt45.svg.png"},
+            {PieceType.BlackRook, "Assets/Sprites/ChessPieces/240x240/240px-Chess_rdt45.svg.png" },
+            {PieceType.BlackKing, "Assets/Sprites/ChessPieces/240x240/240px-Chess_kdt45.svg.png"},
+            {PieceType.BlackQueen, "Assets/Sprites/ChessPieces/240x240/240px-Chess_qdt45.svg.png"},
+            {PieceType.WhitePawn, "Assets/Sprites/ChessPieces/240x240/240px-Chess_plt45.svg.png"},
+            {PieceType.WhiteBishop, "Assets/Sprites/ChessPieces/240x240/240px-Chess_blt45.svg.png"},
+            {PieceType.WhiteKnight, "Assets/Sprites/ChessPieces/240x240/240px-Chess_nlt45.svg.png"},
+            {PieceType.WhiteRook, "Assets/Sprites/ChessPieces/240x240/240px-Chess_rlt45.svg.png"},
+            {PieceType.WhiteKing, "Assets/Sprites/ChessPieces/240x240/240px-Chess_klt45.svg.png"},
+            {PieceType.WhiteQueen, "Assets/Sprites/ChessPieces/240x240/240px-Chess_qlt45.svg.png"}
+
+        };
+    public static IDictionary<PieceType, string> PieceSpriteMap => _pieceSpriteAssetManager; 
+}

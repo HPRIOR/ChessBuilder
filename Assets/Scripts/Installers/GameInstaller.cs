6a633822faff054810f9c55fc63f1bf1d87c2f93
Harry Prior <harryprior@hotmail.co.uk>
2021-03-13 09:11:20 +0000

    Add installer for top level game object

---
+++ Assets/Scripts/Installers/GameInstaller.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+
+public class GameInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        Container.Bind<IBoardGenerator>().To<BoardInitialiser>().AsTransient();
+        Container.Bind<PieceInjector>().AsSingle(); 
+    }
+}

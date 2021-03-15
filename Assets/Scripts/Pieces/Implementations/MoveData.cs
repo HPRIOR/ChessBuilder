df2ee81daeca08cfc90a1410701148d9d22ee321
Harry Prior <harryprior@hotmail.co.uk>
2021-03-15 15:45:11 +0000

    Remove unused private filed for board state

--- Assets/Scripts/Pieces/Implementations/MoveData.cs
+++ Assets/Scripts/Pieces/Implementations/MoveData.cs
@@ -9,17 +9,15 @@ public class MoveData : IMoveData
     public GameObject DisplacedPiece { get;  private set;}
     public GameObject MovedPiece { get;  private set;}
     public Piece MovedPieceComponent { get;  private set;}
-    private IBoardState _boardState;
 
     [Inject]
     public void Construct(GameObject movedPiece, IBoardPosition destination, IBoardState boardState)
     {
-        _boardState = boardState;
         MovedPiece = movedPiece;
         MovedPieceComponent = movedPiece.GetComponent<Piece>();
         InitialBoardPosition = MovedPieceComponent.BoardPosition;
 
-        DisplacedPiece = _boardState.GetTileAt(destination).CurrentPiece?.gameObject;
+        DisplacedPiece = boardState.GetTileAt(destination).CurrentPiece?.gameObject;
         DestinationBoardPosition = destination;
     }
 

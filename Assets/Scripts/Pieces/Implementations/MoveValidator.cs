f4ac8b536a85a407b2d06c20cd194d92e6ff35bb
Harry Prior <harryprior@hotmail.co.uk>
2021-03-16 12:28:07 +0000

    Add overload for ValidateMoveMethod

--- Assets/Scripts/Pieces/Implementations/MoveValidator.cs
+++ Assets/Scripts/Pieces/Implementations/MoveValidator.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using Zenject;
 
 public class MoveValidator : IMoveValidator
 {
+    private IBoardState _boardState;
+
+    [Inject]
+    public MoveValidator(IBoardState boardState)
+    {
+        _boardState = boardState;
+    }
     
     public bool ValidateMove(GameObject piece, IBoardPosition destination)
     {
@@ -14,6 +22,15 @@ public class MoveValidator : IMoveValidator
         return true;
 
         // return PieceCanMove(piece, destination);
+    }
+
+    public bool ValidateMove(IBoardPosition origin, IBoardPosition destination)
+    {
+        if (_boardState.GetTileAt(origin).CurrentPiece == null)
+        {
+            return false;
+        }
+        return true;
     }
 
     private bool PieceCanMove(GameObject piece, IBoardPosition destination) => true;
--- Assets/Scripts/Pieces/Interfaces/IMoveValidator.cs
+++ Assets/Scripts/Pieces/Interfaces/IMoveValidator.cs
@@ -8,4 +8,5 @@ using UnityEngine;
 public interface IMoveValidator
 {
     bool ValidateMove(GameObject piece, IBoardPosition destination);
+    bool ValidateMove(IBoardPosition origin, IBoardPosition destination);
 }

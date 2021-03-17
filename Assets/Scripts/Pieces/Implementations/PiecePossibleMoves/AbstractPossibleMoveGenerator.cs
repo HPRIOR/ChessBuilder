9da78edf5f8b39aa7e89e7190c55b9b7d5028d08
Harry Prior <harryprior@hotmail.co.uk>
2021-03-17 13:10:18 +0000

    Add initial classes for checking moves

---
+++ Assets/Scripts/Pieces/Implementations/PiecePossibleMoves/AbstractPossibleMoveGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public abstract class AbstractPossibleMoveGenerator : IPossibleMoveGenerator
+{
+    protected IBoardState boardState;
+    public AbstractPossibleMoveGenerator(IBoardState boardState)
+    {
+        this.boardState = boardState;
+    }
+
+    protected ITile GetTileAt(int x, int y) => 
+        boardState.GetTileAt(new BoardPosition(x, y));
+
+    public abstract IEnumerable<IBoardPosition> PossibleBoardMoves(GameObject piece);
+}
---
+++ Assets/Scripts/Pieces/Implementations/PiecePossibleMoves/PossibleWhitePawnMoves.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class PossibleWhitePawnMoves : AbstractPossibleMoveGenerator
+{
+
+    public PossibleWhitePawnMoves(IBoardState boardState) : base(boardState){}
+
+    public override IEnumerable<IBoardPosition> PossibleBoardMoves(GameObject piece)
+    {
+        var originPosition = piece.GetComponent<Piece>().BoardPosition;
+        var potentialMoves = new List<IBoardPosition>();
+        
+        if (originPosition.Y == 7) return potentialMoves; // allow to change piece
+
+        var originX = originPosition.X;
+        var originY = originPosition.Y;
+
+        if (GetTileAt(originX, originY + 1).CurrentPiece != null)
+            potentialMoves.Add(new BoardPosition(originX, originY + 1));
+
+        
+        if (originX > 0)
+        {
+            var topLeftTile = GetTileAt(originX - 1, originY + 1);
+            var topLeftPiece = topLeftTile.CurrentPiece?.GetComponent<Piece>();
+            if (topLeftPiece != null && topLeftPiece.PieceColour == PieceColour.Black)
+            {
+                potentialMoves.Add(new BoardPosition(originX - 1, originY + 1));
+            }
+        }
+
+        if (originX <= 7)
+        {
+            var topRightTile = GetTileAt(originX + 1, originY + 1);
+            var topRightPiece = topRightTile.CurrentPiece?.GetComponent<Piece>();
+            if (topRightPiece != null && topRightPiece.PieceColour == PieceColour.Black)
+            {
+                potentialMoves.Add(new BoardPosition(originX + 1, originY + 1));
+            }
+
+        }
+        return potentialMoves;
+    }
+}
+

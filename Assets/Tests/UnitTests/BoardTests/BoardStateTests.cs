425c0f858c996887642cb69fdf0cbef90c72f425
Harry Prior <harryprior@hotmail.co.uk>
2021-03-18 14:56:01 +0000

    Add tests for BoardState

--- Assets/Tests/UnitTests/BoardTests/BoardStateTests.cs
+++ Assets/Tests/UnitTests/BoardTests/BoardStateTests.cs
@@ -10,8 +10,10 @@ public class BoardStateTests : ZenjectUnitTestFixture
         BoardStateInstaller.Install(Container);
     }
 
+    private IBoardState GetBoardState() => Container.Resolve<IBoardState>();
+
     [Test]
-    public void TestBinding()
+    public void BindsCorrectly()
     {
         IBoardState boardState = Container.Resolve<IBoardState>();
 
@@ -19,17 +21,54 @@ public class BoardStateTests : ZenjectUnitTestFixture
     }
 
     [Test]
-    public void TestResolveToClass()
+    public void ResolvesToClass()
     {
         BoardState boardState = Container.Resolve<IBoardState>() as BoardState;
 
         Assert.IsNotNull(boardState);
     }
 
-
-    A
-    public void MyTestMethod()
+    [Test]
+    public void BoardIsGenerated()
     {
+        IBoardState boardState = Container.Resolve<IBoardState>();
 
+        Assert.IsNotNull(boardState.Board);
+    }
+
+    [Test]
+    public void BoardIsCorrectSize()
+    {
+        var boardState = GetBoardState();
+
+        Assert.AreEqual(64, boardState.Board.Length);
+
+    }
+
+    [Test]
+    public void BoardContainsCorrectBoardPositions(
+        [Values(0, 1, 2, 3, 4, 5, 6, 7)] int x, [Values(0, 1, 2, 3, 4, 5, 6, 7)] int y
+        )
+    {
+        var boardState = GetBoardState();
+        Assert.AreEqual(new BoardPosition(x, y), boardState.Board[x, y].BoardPosition);
+    }
+
+    [Test]
+    public void GetterRetrievesCorrectTile(
+        [Values(0, 1, 2, 3, 4, 5, 6, 7)] int x, [Values(0, 1, 2, 3, 4, 5, 6, 7)] int y
+        )
+    {
+        var boardState = GetBoardState();
+        Assert.AreEqual(boardState.GetTileAt(new BoardPosition(x, y)).BoardPosition, new BoardPosition(x, y));
+    }
+
+    [Test]
+    public void BoardContainsNoPiecesOnInit(
+        [Values(0, 1, 2, 3, 4, 5, 6, 7)] int x, [Values(0, 1, 2, 3, 4, 5, 6, 7)] int y
+        )
+    {
+        var boardState = GetBoardState();
+        Assert.IsNull(boardState.GetTileAt(new BoardPosition(x, y)).CurrentPiece);
     }
 }

22c108621a1b00c96dc0cebd9c4ace32b9f2861d
Harry Prior <harryprior@hotmail.co.uk>
2021-03-19 15:36:36 +0000

    Add piece info to Piece class

--- Assets/Scripts/Pieces/Implementations/Piece.cs
+++ Assets/Scripts/Pieces/Implementations/Piece.cs
@@ -8,30 +8,31 @@ public class Piece : MonoBehaviour
     public IBoardPosition BoardPosition { get; set; }
     public PieceColour PieceColour { get; }
     public PieceType PieceType { get; set; }
-
+    public IPieceInfo PieceInfo { get; private set; }
     public IPossibleMoveGenerator possibleMoveGenerator;
 
-    private static IDictionary<PieceType, string> _spriteAssetMap = PieceSpriteAssetManager.PieceSpriteMap; 
     private SpriteRenderer _spriteRenderer;
 
     private void Start()
     {
         gameObject.transform.parent = GameObject.FindGameObjectWithTag("Pieces").transform;
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        _spriteRenderer.sprite = AssetDatabase.LoadAssetAtPath<Sprite>(_spriteAssetMap[PieceType]);
+        _spriteRenderer.sprite = AssetDatabase.LoadAssetAtPath<Sprite>(PieceSpriteAssetManager.GetSpriteAsset(PieceType));
         gameObject.transform.position = BoardPosition.Position;
+        Debug.Log(PieceColour);
     }
 
-    public IEnumerable<IBoardPosition> GetPossibleMoves() => possibleMoveGenerator.GetPossibleBoardMoves(gameObject);
+    public IEnumerable<IBoardPosition> GetPossiblePieceMoves() => possibleMoveGenerator.GetPossiblePieceMoves(gameObject);
 
     [Inject]
-    public void Construct(PieceType pieceType, IBoardPosition boardPosition, IPossibleMoveGeneratorFactory possibleMoveGeneratorFactory)
+    public void Construct(PieceType pieceType, IPieceInfo pieceInfo, IBoardPosition boardPosition, IPossibleMoveGeneratorFactory possibleMoveGeneratorFactory)
     {
+        PieceInfo = pieceInfo;
         PieceType = pieceType;
         BoardPosition = boardPosition;
         possibleMoveGenerator = possibleMoveGeneratorFactory.GetPossibleMoveGenerator(PieceType);
     }
     
-    public class Factory : PlaceholderFactory<PieceType, IBoardPosition, Piece> { }
+    public class Factory : PlaceholderFactory<PieceType, IPieceInfo, IBoardPosition, Piece> { }
 
 }

2e04e02a958ada919486493e2eefadedf609ecc6
Harry Prior <harryprior@hotmail.co.uk>
2021-03-19 15:33:28 +0000

    Add installer for possible move generator

---
+++ Assets/Scripts/Bindings/Installers/PieceInstallers/PossibleBoardMovesGeneratorInstaller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+
+public class PossibleBoardMovesGeneratorInstaller : Installer<PossibleBoardMovesGeneratorInstaller>
+{
+    public override void InstallBindings()
+    {
+        Container.Bind<IPossibleBoardMovesGenerator>().To<PossibleBoardMovesGenerator>().AsSingle();
+    }
+}

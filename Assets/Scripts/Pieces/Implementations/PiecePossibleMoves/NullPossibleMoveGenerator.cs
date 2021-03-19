19730874c930b4e870303875fcb5a01c1d550c83
Harry Prior <harryprior@hotmail.co.uk>
2021-03-19 11:58:40 +0000

    use null object pattern for non pieces

---
+++ Assets/Scripts/Pieces/Implementations/PiecePossibleMoves/NullPossibleMoveGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class NullPossibleMoveGenerator : IPossibleMoveGenerator
+{
+    public IEnumerable<IBoardPosition> PossibleBoardMoves(GameObject piece) => new List<IBoardPosition>();
+}
+

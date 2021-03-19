828349d1e24d484c84aa8eb27e1756401c96ee5b
Harry Prior <harryprior@hotmail.co.uk>
2021-03-19 15:38:44 +0000

    #

--- Assets/Scripts/Game/Game.cs
+++ Assets/Scripts/Game/Game.cs
@@ -8,19 +8,21 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    private IBoardState _boardState;
     private IPieceSpawner _pieceInjector;
+    private IPossibleBoardMovesGenerator _possibleBoardMovesGenerator;
 
     [Inject]
-    public void Construct(IBoardState boardState, IPieceSpawner pieceInjector)
+    public void Construct(IPieceSpawner pieceInjector, IPossibleBoardMovesGenerator possibleBoardMovesGenerator)
     {
-        _boardState = boardState;
         _pieceInjector = pieceInjector;
+        _possibleBoardMovesGenerator = possibleBoardMovesGenerator;
     }
 
     public void Start()
     {
-        _pieceInjector.CreatePieceOf(PieceType.BlackKing, new BoardPosition(0, 0));
-        _pieceInjector.CreatePieceOf(PieceType.WhiteKing, new BoardPosition(7, 7));
+        //_pieceInjector.CreatePieceOf(PieceType.BlackKing, new BoardPosition(0, 0));
+        //_pieceInjector.CreatePieceOf(PieceType.WhiteKing, new BoardPosition(7, 7));
+        _pieceInjector.CreatePieceOf(PieceType.WhitePawn, new BoardPosition(0, 1));
+        _possibleBoardMovesGenerator.GeneratePossibleMoves();
     }
 }

bbf84c225dcb6711256951f56e0223f2c8e8a718
Harry Prior <harryprior@hotmail.co.uk>
2021-03-19 15:34:36 +0000

    Add mechanism for deciding PieceColour of PieceType

---
+++ Assets/Scripts/Pieces/Implementations/PieceInfo/PieceColourMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+static class PieceColourMap
+{
+    private static HashSet<PieceType> WhitePieces = new HashSet<PieceType>() { 
+        PieceType.WhiteBishop,
+        PieceType.WhiteKnight,
+        PieceType.WhitePawn,
+        PieceType.WhiteQueen,
+        PieceType.WhiteRook,
+        PieceType.WhiteKing
+    };
+    public static PieceColour GetPieceColour(PieceType pieceType) => WhitePieces.Contains(pieceType) ? 
+        PieceColour.White : PieceColour.Black;
+}

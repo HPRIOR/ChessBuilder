5b9e8d625b6f901f99befafb066983bdbb80d3be
Harry Prior <harryprior@hotmail.co.uk>
2021-03-19 15:32:53 +0000

    Add installer for move validator

---
+++ Assets/Scripts/Bindings/Installers/PieceInstallers/MoveValidatorInstaller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+
+public class MoveValidatorInstaller : Installer<MoveValidatorInstaller>
+{
+    public override void InstallBindings()
+    {
+        Container.Bind<IMoveValidator>().To<MoveValidator>().AsSingle();
+    }
+}

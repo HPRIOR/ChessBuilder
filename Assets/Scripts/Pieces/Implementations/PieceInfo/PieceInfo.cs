1f664e07141ab474c989e8fbcbcee5cae2b655cd
Harry Prior <harryprior@hotmail.co.uk>
2021-03-19 15:34:53 +0000

    Add Piece Info class

---
+++ Assets/Scripts/Pieces/Implementations/PieceInfo/PieceInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class PieceInfo : IPieceInfo
+{
+    public PieceInfo(PieceType pieceType)
+    {
+        SpriteAsset = PieceSpriteAssetManager.GetSpriteAsset(pieceType);
+        PieceColour = PieceColourMap.GetPieceColour(pieceType);
+        PieceType = pieceType;
+    }
+    public string SpriteAsset { get; }
+    public PieceType PieceType { get; }
+    public PieceColour PieceColour { get; }
+}

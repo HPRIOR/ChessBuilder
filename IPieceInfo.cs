2570548b111ab1d47f284883780439e39b52aa00
Harry Prior <harryprior@hotmail.co.uk>
2021-03-19 15:38:39 +0000

    Add interface for pieceinfo

---
+++ IPieceInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface IPieceInfo : IPieceInfo
+{
+    public string SpriteAsset { get; }
+    public PieceColour PieceColour { get; }
+    public PieceType PieceType { get; }
+}

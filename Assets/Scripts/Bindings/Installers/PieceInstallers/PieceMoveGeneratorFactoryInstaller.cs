81d3a8cee8e764b872a7cc9480962db8adec7aa0
Harry Prior <harryprior@hotmail.co.uk>
2021-03-21 15:49:12 +0000

    rename

--- Assets/Scripts/Bindings/Installers/PieceInstallers/PieceMoveGeneratorFactoryInstaller.cs
+++ Assets/Scripts/Bindings/Installers/PieceInstallers/PieceMoveGeneratorFactoryInstaller.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class PossibleMoveGeneratorFactoryInstaller : Installer<PossibleMoveGeneratorFactoryInstaller>
+public class PieceMoveGeneratorFactoryInstaller : Installer<PieceMoveGeneratorFactoryInstaller>
 {
     public override void InstallBindings()
     {

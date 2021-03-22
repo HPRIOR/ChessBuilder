570fdf1fa100a25f78b37744e1c6a81f73b93bdd
Harry Prior <harryprior@hotmail.co.uk>
2021-03-22 14:16:47 +0000

    Remove unused using statements

--- Assets/Scripts/Bindings/Installers/PieceInstallers/PieceMoveGeneratorFactoryInstaller.cs
+++ Assets/Scripts/Bindings/Installers/PieceInstallers/PieceMoveGeneratorFactoryInstaller.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using Zenject;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Zenject;
 
 public class PieceMoveGeneratorFactoryInstaller : Installer<PieceMoveGeneratorFactoryInstaller>
 {

62919d688237215ce70478d947b1e652f48cc850
Harry Prior <harryprior@hotmail.co.uk>
2021-03-22 14:17:43 +0000

    Add methods for allowing black and white pieces to be tested with the same logic

--- Assets/Tests/UnitTests/PieceMoveTests/PossibleMoveTestBase.cs
+++ Assets/Tests/UnitTests/PieceMoveTests/PossibleMoveTestBase.cs
@@ -3,12 +3,14 @@ using System.Linq;
 using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PossibleMoveTestBase : ZenjectUnitTestFixture
 {
     private IPieceSpawner _pieceSpawner;
     private IPieceMoveGeneratorFactory _pieceMoveGeneratorFactory;
     private IBoardState _boardState;
+    protected PieceColour TestedPieceColour { get; set; } = PieceColour.White;
 
     [SetUp]
     public void Init()
@@ -20,6 +22,7 @@ public class PossibleMoveTestBase : ZenjectUnitTestFixture
     [TearDown]
     public void TearDown()
     {
+        Container.UnbindAll();
         _boardState = null;
         _pieceMoveGeneratorFactory = null;
         _pieceSpawner = null;
@@ -42,8 +45,50 @@ public class PossibleMoveTestBase : ZenjectUnitTestFixture
     }
 
     protected void SetUpBoardWith(IEnumerable<(PieceType piece, IBoardPosition boardPosition)> piecesAtPositions) =>
-        piecesAtPositions.ToList().ForEach(item => _pieceSpawner.CreatePieceOf(item.piece, item.boardPosition));
-    protected GameObject GetGameObjectAtPosition(int x, int y) => _boardState.GetTileAt(new BoardPosition(x, y)).CurrentPiece;
-    protected IPieceMoveGenerator GetPossibleMoveGenerator(PieceType pieceType) => _pieceMoveGeneratorFactory.GetPossibleMoveGenerator(pieceType);
+        piecesAtPositions
+        .ToList()
+        .ForEach(item =>
+            _pieceSpawner.CreatePieceOf(
+                item.piece, item.boardPosition)
+        );
+
+
+    protected GameObject GetGameObjectAtPosition(int x, int y) => TestedPieceColour == PieceColour.White 
+        ? _boardState.GetTileAt(new BoardPosition(x, y)).CurrentPiece 
+        : _boardState.GetMirroredTileAt(new BoardPosition(x, y)).CurrentPiece;
+
+    protected IPieceMoveGenerator GetPossibleMoveGenerator(PieceType pieceType) => 
+        _pieceMoveGeneratorFactory.GetPossibleMoveGenerator(pieceType);
+
+    protected PieceType GetOppositePieceType(PieceType pieceType)
+    {
+        var pieceTypeString = pieceType.ToString();
+        if (pieceTypeString.StartsWith("White"))
+        {
+            return (PieceType)Enum.Parse(typeof(PieceType), "Black" + pieceTypeString.Substring(5));
+        }
+        return (PieceType)Enum.Parse(typeof(PieceType), "White" + pieceTypeString.Substring(5));
+    }
+
+    /// <summary>
+    /// Gets the position relative to the current tested piece (white or black)
+    /// </summary>
+    /// <param name="boardPosition"></param>
+    /// <returns></returns>
+    protected IBoardPosition GetRelativePosition(IBoardPosition boardPosition) =>
+        TestedPieceColour == PieceColour.White ? boardPosition : GetMirroredBoardPosition(boardPosition);
+
+   
+    private IBoardPosition GetMirroredBoardPosition(IBoardPosition boardPosition) =>
+        new BoardPosition(Math.Abs(boardPosition.X - 7), Math.Abs(boardPosition.Y - 7));
+
+
+    protected void SetTestedPieceColourWith(PieceType currentPieceType) =>
+        _ = TestedPieceColour = GetPieceColourFrom(currentPieceType);
+
+    private PieceColour GetPieceColourFrom(PieceType pieceType) => 
+        pieceType.ToString().StartsWith("White") ? PieceColour.White : PieceColour.Black;
+
+
 
 }

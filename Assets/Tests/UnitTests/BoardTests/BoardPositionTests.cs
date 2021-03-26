7a55c644bbb42e4b44638d823e4fb8ff0e69baf8
Harry Prior <harryprior@hotmail.co.uk>
2021-03-26 09:58:53 +0000

    Add tests for boardposition arithmatic

---
+++ Assets/Tests/UnitTests/BoardTests/BoardPositionTests.cs
@@ -0,0 +1,82 @@
+using Zenject;
+using NUnit.Framework;
+
+[TestFixture]
+public class BoardPositionTests : ZenjectUnitTestFixture
+{
+    [Test]
+    public void AddNorth()
+    {
+        var boardPositionOne = new BoardPosition(1, 1);
+        var boardPositionTwo = new BoardPosition(0, 1);
+        var boardPositionSum = boardPositionOne.Add(boardPositionTwo);
+        Assert.AreEqual(new BoardPosition(1, 2), boardPositionSum);
+    }
+    
+    [Test]
+    public void AddSouth()
+    {
+        var boardPositionOne = new BoardPosition(1, 1);
+        var boardPositionTwo = new BoardPosition(0, -1);
+        var boardPositionSum = boardPositionOne.Add(boardPositionTwo);
+        Assert.AreEqual(new BoardPosition(1, 0), boardPositionSum);
+    }
+
+    [Test]
+    public void AddEast()
+    {
+        var boardPositionOne = new BoardPosition(1, 1);
+        var boardPositionTwo = new BoardPosition(1, 0);
+        var boardPositionSum = boardPositionOne.Add(boardPositionTwo);
+        Assert.AreEqual(new BoardPosition(2, 1), boardPositionSum);
+    }
+
+    [Test]
+    public void AddWest()
+    {
+        var boardPositionOne = new BoardPosition(1, 1);
+        var boardPositionTwo = new BoardPosition(-1, 0);
+        var boardPositionSum = boardPositionOne.Add(boardPositionTwo);
+        Assert.AreEqual(new BoardPosition(0, 1), boardPositionSum);
+    }
+
+    [Test]
+    public void AddNorthEast()
+    {
+        var boardPositionOne = new BoardPosition(1, 1);
+        var boardPositionTwo = new BoardPosition(1, 1);
+        var boardPositionSum = boardPositionOne.Add(boardPositionTwo);
+        Assert.AreEqual(new BoardPosition(2, 2), boardPositionSum);
+    }
+
+    [Test]
+    public void AddNorthWest()
+    {
+        var boardPositionOne = new BoardPosition(1, 1);
+        var boardPositionTwo = new BoardPosition(-1, 1);
+        var boardPositionSum = boardPositionOne.Add(boardPositionTwo);
+        Assert.AreEqual(new BoardPosition(0, 2), boardPositionSum);
+    }
+
+    [Test]
+    public void AddSouthEast()
+    {
+        var boardPositionOne = new BoardPosition(1, 1);
+        var boardPositionTwo = new BoardPosition(1, -1);
+        var boardPositionSum = boardPositionOne.Add(boardPositionTwo);
+        Assert.AreEqual(new BoardPosition(2, 0), boardPositionSum);
+    }
+
+    [Test]
+    public void AddSouthWest()
+    {
+        var boardPositionOne = new BoardPosition(1, 1);
+        var boardPositionTwo = new BoardPosition(-1, -1);
+        var boardPositionSum = boardPositionOne.Add(boardPositionTwo);
+        Assert.AreEqual(new BoardPosition(0, 0), boardPositionSum);
+    }
+
+
+
+
+}

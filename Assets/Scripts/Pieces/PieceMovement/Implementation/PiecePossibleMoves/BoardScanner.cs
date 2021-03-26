54a0e17d07334eae46fa566da36ce28e1dd1f4f2
Harry Prior <harryprior@hotmail.co.uk>
2021-03-26 10:33:58 +0000

    Add classes to implement transition to composition in the design of possible move generator

---
+++ Assets/Scripts/Pieces/PieceMovement/Implementation/PiecePossibleMoves/BoardScanner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class BoardScanner : IBoardScanner
+{
+    public IEnumerable<IBoardPosition> ScanIn(Direction direction, IBoardPosition fromPosition)
+    {
+        throw new NotImplementedException();
+    }
+}

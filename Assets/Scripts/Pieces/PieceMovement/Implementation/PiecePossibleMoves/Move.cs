d1e25733a553a2b9e188f8f04ba4c7c87de751df
Harry Prior <harryprior@hotmail.co.uk>
2021-03-26 09:57:40 +0000

    Add class for handling movement in a particular direction

---
+++ Assets/Scripts/Pieces/PieceMovement/Implementation/PiecePossibleMoves/Move.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class Move
+{
+    private static IDictionary<Direction, IBoardPosition> directionMovement = new Dictionary<Direction, IBoardPosition>()
+    {
+        {Direction.N, new BoardPosition(0 , 1)},
+        {Direction.E, new BoardPosition(1 , 0)},
+        {Direction.S, new BoardPosition(0 , -1)},
+        {Direction.W, new BoardPosition(-1 , 0)},
+        {Direction.NE, new BoardPosition(1 , 1)},
+        {Direction.SE, new BoardPosition(1 , -1)},
+        {Direction.SW, new BoardPosition(-1 , -1)},
+        {Direction.NW, new BoardPosition(-1 , 1)},
+    };
+
+    public static IBoardPosition In(Direction direction) => directionMovement[direction];
+
+}

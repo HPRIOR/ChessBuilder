a9fc026add6c3b2bd7cd38c7e63ae0deb79ebb2d
Harry Prior <harryprior@hotmail.co.uk>
2021-03-26 17:59:41 +0000

    Rename var and add final tests

--- Assets/Tests/UnitTests/PieceMoveTests/PossibleRookMovesTests.cs
+++ Assets/Tests/UnitTests/PieceMoveTests/PossibleRookMovesTests.cs
@@ -1,4 +1,3 @@
-using Zenject;
 using NUnit.Framework;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +15,7 @@ public class PossibleRookMovesTests : PossibleMovesTestBase
         SetTestedPieceColourWith(pieceType);
         var rookMoveGenerator = GetPossibleMoveGenerator(pieceType);
         var pieces = new List<(PieceType, IBoardPosition)>() {
-            (pieceType, GetRelativePosition(new BoardPosition(x, y)))
+            (pieceType, RelativePositionToTestedPiece(new BoardPosition(x, y)))
         };
 
         SetUpBoardWith(pieces);
@@ -24,7 +23,7 @@ public class PossibleRookMovesTests : PossibleMovesTestBase
         var rookGameObject = GetGameObjectAtPosition(x, y);
         var possibleMoves = rookMoveGenerator.GetPossiblePieceMoves(rookGameObject);
 
-        Assert.AreEqual(14, possibleMoves.Count());
+        Assert.AreEqual(14, new HashSet<IBoardPosition>(possibleMoves).Count());
     }
 
     [Test]
@@ -38,9 +37,9 @@ public class PossibleRookMovesTests : PossibleMovesTestBase
             SetTestedPieceColourWith(pieceType);
             var rookMoveGenerator = GetPossibleMoveGenerator(pieceType);
             var pieces = new List<(PieceType, IBoardPosition)>() {
-            (pieceType, GetRelativePosition(new BoardPosition(x, y))),
-            (GetOppositePieceType(pieceType), GetRelativePosition(new BoardPosition(x, 7))),
-            (GetOppositePieceType(pieceType), GetRelativePosition(new BoardPosition(7, y))),
+                (pieceType, RelativePositionToTestedPiece(new BoardPosition(x, y))),
+                (GetOppositePieceType(pieceType), RelativePositionToTestedPiece(new BoardPosition(x, 7))),
+                (GetOppositePieceType(pieceType), RelativePositionToTestedPiece(new BoardPosition(7, y))),
             };
 
             SetUpBoardWith(pieces);
@@ -48,7 +47,7 @@ public class PossibleRookMovesTests : PossibleMovesTestBase
             var rookGameObject = GetGameObjectAtPosition(x, y);
             var possibleMoves = rookMoveGenerator.GetPossiblePieceMoves(rookGameObject);
 
-            Assert.AreEqual(14, possibleMoves.Count());
+            Assert.AreEqual(14, new HashSet<IBoardPosition>(possibleMoves).Count());
         }
         catch (PieceSpawnException)
         {
@@ -58,7 +57,7 @@ public class PossibleRookMovesTests : PossibleMovesTestBase
 
     [Test]
     public void WithOpposingPiecesOnSeventhRankAndFile_RookCanTakeAndIsBlocked(
-        [Values(0, 1, 2, 3, 4, 5, 7)] int x, [Values(0, 1, 2, 3, 4, 5, 7)] int y,
+        [Values(0, 1, 2, 3, 4, 5)] int x, [Values(0, 1, 2, 3, 4, 5)] int y,
         [Values(PieceType.WhiteRook, PieceType.BlackRook)] PieceType pieceType
         )
     {
@@ -67,9 +66,9 @@ public class PossibleRookMovesTests : PossibleMovesTestBase
             SetTestedPieceColourWith(pieceType);
             var rookMoveGenerator = GetPossibleMoveGenerator(pieceType);
             var pieces = new List<(PieceType, IBoardPosition)>() {
-            (pieceType, GetRelativePosition(new BoardPosition(x, y))),
-            (GetOppositePieceType(pieceType), GetRelativePosition(new BoardPosition(x, 6))),
-            (GetOppositePieceType(pieceType), GetRelativePosition(new BoardPosition(6, y))),
+                (pieceType, RelativePositionToTestedPiece(new BoardPosition(x, y))),
+                (GetOppositePieceType(pieceType), RelativePositionToTestedPiece(new BoardPosition(x, 6))),
+                (GetOppositePieceType(pieceType), RelativePositionToTestedPiece(new BoardPosition(6, y))),
             };
 
             SetUpBoardWith(pieces);
@@ -80,7 +79,7 @@ public class PossibleRookMovesTests : PossibleMovesTestBase
             IList<IBoardPosition> unreachableTiles = new List<IBoardPosition>()
             {
                 new BoardPosition(x, 7), new BoardPosition(y, 7)
-            }.Select(GetRelativePosition).ToList();
+            }.Select(RelativePositionToTestedPiece).ToList();
 
             HashSet<IBoardPosition> reachableTiles = new HashSet<IBoardPosition>(possibleMoves);
 
@@ -93,7 +92,117 @@ public class PossibleRookMovesTests : PossibleMovesTestBase
     }
 
     [Test]
+    public void WithOpposingPiecesOnMidRankAndFile_RookCanTakeAndIsBlocked(
+        [Values(0, 1, 2)] int x, [Values(0, 1, 2)] int y,
+        [Values(PieceType.WhiteRook, PieceType.BlackRook)] PieceType pieceType
+        )
+    {
+        try
+        {
+            SetTestedPieceColourWith(pieceType);
+            var rookMoveGenerator = GetPossibleMoveGenerator(pieceType);
+            var pieces = new List<(PieceType, IBoardPosition)>() {
+                (pieceType, RelativePositionToTestedPiece(new BoardPosition(x, y))),
+                (GetOppositePieceType(pieceType), RelativePositionToTestedPiece(new BoardPosition(x, 3))),
+                (GetOppositePieceType(pieceType), RelativePositionToTestedPiece(new BoardPosition(3, y))),
+            };
+
+            SetUpBoardWith(pieces);
+
+            var rookGameObject = GetGameObjectAtPosition(x, y);
+            var possibleMoves = rookMoveGenerator.GetPossiblePieceMoves(rookGameObject);
+
+            var unreachableTilesNorth = GetPositionsIncludingAndPassed(new BoardPosition(x, 4), Direction.N).ToList();
+            var unreachableTilesEast = GetPositionsIncludingAndPassed(new BoardPosition(4, y), Direction.E).ToList();
+
+            // Todo check all angles as in with friendly test of the same angle
+
+            var unreachableTiles = unreachableTilesEast.Concat(unreachableTilesNorth);
+
+            HashSet<IBoardPosition> reachableTiles = new HashSet<IBoardPosition>(possibleMoves);
+
+            Assert.IsFalse(reachableTiles.Overlaps(unreachableTiles));
+        }
+        catch (PieceSpawnException)
+        {
+            Debug.Log("Test skipped due to spawn clash");
+        }
+    }
+
+
+    [Test]
     public void WithFriendlyPieceOnBackRankAndFile_RookIsBlocked(
+        [Values(0, 1, 2, 3, 4, 5, 6)] int x, [Values(0, 1, 2, 3, 4, 5, 6)] int y,
+        [Values(PieceType.WhiteRook, PieceType.BlackRook)] PieceType pieceType
+        )
+    {
+        try
+        {
+            SetTestedPieceColourWith(pieceType);
+            var rookMoveGenerator = GetPossibleMoveGenerator(pieceType);
+            var pieces = new List<(PieceType, IBoardPosition)>() {
+                (pieceType, RelativePositionToTestedPiece(new BoardPosition(x, y))),
+                (pieceType, RelativePositionToTestedPiece(new BoardPosition(x, 7))),
+                (pieceType, RelativePositionToTestedPiece(new BoardPosition(7, y))),
+            };
+
+            SetUpBoardWith(pieces);
+
+            var rookGameObject = GetGameObjectAtPosition(x, y);
+            var possibleMoves = rookMoveGenerator.GetPossiblePieceMoves(rookGameObject);
+
+            var reachableTile = new HashSet<IBoardPosition>(possibleMoves);
+            var unreachableTiles =
+                new HashSet<IBoardPosition>(
+                    new List<IBoardPosition>() { new BoardPosition(7, y), new BoardPosition(x, 7) }
+                    .Select(RelativePositionToTestedPiece));
+
+            Assert.IsFalse(reachableTile.Overlaps(unreachableTiles));
+        }
+        catch (PieceSpawnException)
+        {
+            Debug.Log("Test skipped due to spawn clash");
+        }
+    }
+
+    [Test]
+    public void WithFriendlyPieceOnSeventhRankAndFile_RookIsBlocked(
+        [Values(0, 1, 2, 3, 4, 5)] int x, [Values(0, 1, 2, 3, 4, 5)] int y,
+        [Values(PieceType.WhiteRook, PieceType.BlackRook)] PieceType pieceType
+        )
+    {
+        try
+        {
+            SetTestedPieceColourWith(pieceType);
+            var rookMoveGenerator = GetPossibleMoveGenerator(pieceType);
+            var pieces = new List<(PieceType, IBoardPosition)>() {
+                (pieceType, RelativePositionToTestedPiece(new BoardPosition(x, y))),
+                (pieceType, RelativePositionToTestedPiece(new BoardPosition(x, 6))),
+                (pieceType, RelativePositionToTestedPiece(new BoardPosition(6, y))),
+            };
+
+            SetUpBoardWith(pieces);
+
+            var rookGameObject = GetGameObjectAtPosition(x, y);
+            var possibleMoves = rookMoveGenerator.GetPossiblePieceMoves(rookGameObject);
+
+            var reachableTile = new HashSet<IBoardPosition>(possibleMoves);
+            var unreachableTilesNorth =
+                GetPositionsIncludingAndPassed(new BoardPosition(x, 6), Direction.N).ToList();
+            var unreachableTilesEast =
+                GetPositionsIncludingAndPassed(new BoardPosition(6, y), Direction.E).ToList();
+            var unreachableTiles = unreachableTilesEast.Concat(unreachableTilesNorth).Select(RelativePositionToTestedPiece);
+
+            Assert.IsFalse(reachableTile.Overlaps(unreachableTiles));
+        }
+        catch (PieceSpawnException)
+        {
+            Debug.Log("Test skipped due to spawn clash");
+        }
+    }
+
+    [Test]
+    public void WithFriendlyPiecesOnMidRankAndFile_RookIsBlocked(
         [Values(0, 1, 2, 3, 4, 5, 6, 7)] int x, [Values(0, 1, 2, 3, 4, 5, 6, 7)] int y,
         [Values(PieceType.WhiteRook, PieceType.BlackRook)] PieceType pieceType
         )
@@ -103,9 +212,9 @@ public class PossibleRookMovesTests : PossibleMovesTestBase
             SetTestedPieceColourWith(pieceType);
             var rookMoveGenerator = GetPossibleMoveGenerator(pieceType);
             var pieces = new List<(PieceType, IBoardPosition)>() {
-            (pieceType, GetRelativePosition(new BoardPosition(x, y))),
-            (pieceType, GetRelativePosition(new BoardPosition(x, 7))),
-            (pieceType, GetRelativePosition(new BoardPosition(7, y))),
+                (pieceType, RelativePositionToTestedPiece(new BoardPosition(x, y))),
+                (pieceType, RelativePositionToTestedPiece(new BoardPosition(x, 3))),
+                (pieceType, RelativePositionToTestedPiece(new BoardPosition(3, y))),
             };
 
             SetUpBoardWith(pieces);
@@ -113,18 +222,32 @@ public class PossibleRookMovesTests : PossibleMovesTestBase
             var rookGameObject = GetGameObjectAtPosition(x, y);
             var possibleMoves = rookMoveGenerator.GetPossiblePieceMoves(rookGameObject);
 
-            var reachableTile = new HashSet<IBoardPosition>(possibleMoves);
-            var unreachableTiles = 
-                new HashSet<IBoardPosition>(
-                    new List<IBoardPosition>(){ new BoardPosition(7, y), new BoardPosition(x, 7)}
-                    .Select(GetRelativePosition));
+            var unreachableTilesNorth = GetPositionsIncludingAndPassed(new BoardPosition(x, 3), Direction.N).ToList();
+            var unreachableTilesEast = GetPositionsIncludingAndPassed(new BoardPosition(3, y), Direction.E).ToList();
 
-            Assert.IsFalse(reachableTile.Overlaps(unreachableTiles));
+            var unreachableTilesSouth = GetPositionsIncludingAndPassed(new BoardPosition(x, 3), Direction.S).ToList();
+            var unreachabletilesWest = GetPositionsIncludingAndPassed(new BoardPosition(3, y), Direction.W).ToList();
+
+
+            var unreachableTiles =
+                x < 3 
+                ? unreachableTilesEast 
+                : unreachabletilesWest
+                .Concat(
+                    y < 3 
+                    ? unreachableTilesNorth 
+                    : unreachabletilesWest);
+
+
+            HashSet<IBoardPosition> reachableTiles = new HashSet<IBoardPosition>(possibleMoves);
+
+            Assert.IsFalse(reachableTiles.Overlaps(unreachableTiles));
         }
         catch (PieceSpawnException)
         {
             Debug.Log("Test skipped due to spawn clash");
         }
     }
+
 
 }

cdaacbfc5197fde7bb0a8c7549bf29c43f2a6f18
Harry Prior <harryprior@hotmail.co.uk>
2021-04-25 17:58:52 +0100

    Comments

--- Assets/Scripts/Controllers/Commands/MoveCommand.cs
+++ Assets/Scripts/Controllers/Commands/MoveCommand.cs
@@ -38,15 +38,17 @@ public class MoveCommand : ICommand
         if (_from == _destination) return false;
         if (_moveValidator.ValidateMove(_gameState.PossiblePieceMoves, _from, _destination))
             return true;
-
+        
+        // return to original state
         _gameState.UpdateGameState(_stateTransitionedFrom);
         return false;
     }
 
-    public void Undo()
-    {
+    // If updategamestate is changed to accept the changed tile, it is not clear how undo will work
+    // the move which instigated _stateTransitioned from is lost
+    // some hashing may need to be implemented, so that states can be saved along with corresponding moves 
+    public void Undo() =>
         _gameState.UpdateGameState(_stateTransitionedFrom);
-    }
 
     public class Factory : PlaceholderFactory<IBoardPosition, IBoardPosition, MoveCommand> { }
 }

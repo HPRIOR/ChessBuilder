1c29b771bfc34c75ee8b3d1937eb284b3dba6253
Harry Prior <harryprior@hotmail.co.uk>
2021-05-22 21:27:14 +0100

    Add init tests for game state controller

--- Assets/Tests/UnitTests/Game/GameStateControllerTests.cs
+++ Assets/Tests/UnitTests/Game/GameStateControllerTests.cs
@@ -1,3 +1,9 @@
+using System;
+using Bindings.Utils;
+using Game.Interfaces;
+using Models.Services.Interfaces;
+using Models.State.Board;
+using Models.State.Interfaces;
 using NUnit.Framework;
 using Zenject;
 
@@ -19,12 +25,47 @@ namespace Tests.UnitTests.Game
             Container.UnbindAll();
         }
 
+        private IGameState _gameStateController;
+        private IBoardGenerator _boardGenerator;
+
         private void InstallBindings()
         {
+            BindAllDefault.InstallAllTo(Container);
         }
 
         private void ResolveContainer()
         {
+            _gameStateController = Container.Resolve<IGameState>();
+            _boardGenerator = Container.Resolve<IBoardGenerator>();
+        }
+
+        [Test]
+        public void HasNullBoardState_WhenInitialised()
+        {
+            Assert.IsNull(_gameStateController.CurrentBoardState);
+        }
+
+        [Test]
+        public void BoardStateIsUpdated_WhenPassedBoardState()
+        {
+            var boardState = new BoardState(_boardGenerator);
+            _gameStateController.UpdateGameState(boardState);
+
+            Assert.AreSame(boardState, _gameStateController.CurrentBoardState);
+        }
+
+        [Test]
+        public void EventInvoked_WhenStateUpdated()
+        {
+            var turnEventInvoker = _gameStateController as ITurnEventInvoker;
+            var count = 0;
+            Action<IBoardState, IBoardState> mockFunc = (prev, newState) => { count += 1; };
+            turnEventInvoker.GameStateChangeEvent += mockFunc;
+
+            var boardState = new BoardState(_boardGenerator);
+            _gameStateController.UpdateGameState(boardState);
+
+            Assert.AreEqual(1, count);
         }
     }
 }

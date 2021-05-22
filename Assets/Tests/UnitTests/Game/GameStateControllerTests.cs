9e1d6162757ae6133d4c7d378d2d5c06f3206bb8
Harry Prior <harryprior@hotmail.co.uk>
2021-05-22 20:35:26 +0100

    Add namespace and rename class/file

---
+++ Assets/Tests/UnitTests/Game/GameStateControllerTests.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using Zenject;
+
+namespace Tests.UnitTests.Game
+{
+    [TestFixture]
+    public class GameStateControllerTests : ZenjectUnitTestFixture
+    {
+        [SetUp]
+        public void Init()
+        {
+            InstallBindings();
+            ResolveContainer();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Container.UnbindAll();
+        }
+
+        private void InstallBindings()
+        {
+        }
+
+        private void ResolveContainer()
+        {
+        }
+    }
+}

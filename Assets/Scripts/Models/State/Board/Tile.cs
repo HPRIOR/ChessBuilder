10897b161873cfa1c6db6fc181230767df433ecf
Harry Prior <harryprior@hotmail.co.uk>
2021-05-22 17:27:30 +0100

    make constructor used in cloning private

--- Assets/Scripts/Models/State/Board/Tile.cs
+++ Assets/Scripts/Models/State/Board/Tile.cs
@@ -6,9 +6,9 @@ namespace Models.State.Board
     public class Tile : ITile
     {
         public Piece CurrentPiece { get; set; }
-        public IBoardPosition BoardPosition { get; set; }
+        public IBoardPosition BoardPosition { get;  }
 
-        public Tile(IBoardPosition boardPosition, Piece currentPiece)
+        private Tile(IBoardPosition boardPosition, Piece currentPiece)
         {
             BoardPosition = boardPosition;
             CurrentPiece = currentPiece;

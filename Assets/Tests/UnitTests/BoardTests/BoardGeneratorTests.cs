bb7c94ac707ad07352f734f2e6fbe96f05f56ced
Harry Prior <harryprior@hotmail.co.uk>
2021-05-22 21:39:17 +0100

    Add init boardGeneratorTests

---
+++ Assets/Tests/UnitTests/BoardTests/BoardGeneratorTests.cs
@@ -0,0 +1,44 @@
+using Bindings.Installers.BoardInstallers;
+using Models.Services.Interfaces;
+using Models.State.PieceState;
+using NUnit.Framework;
+using Zenject;
+
+namespace Tests.UnitTests.BoardTests
+{
+    [TestFixture]
+    public class BoardGeneratorTests : ZenjectUnitTestFixture
+    {
+        [SetUp]
+        public void Init()
+        {
+            InstallBindings();
+            ResolveContainer();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Container.UnbindAll();
+        }
+
+        private IBoardGenerator _boardGenerator;
+
+        private void InstallBindings()
+        {
+            BoardGeneratorInstaller.Install(Container);
+        }
+
+        private void ResolveContainer()
+        {
+            _boardGenerator = Container.Resolve<IBoardGenerator>();
+        }
+
+        [Test]
+        public void BoardIsGenerated_WithNullPiecesInTiles()
+        {
+            var board = _boardGenerator.GenerateBoard();
+            foreach (var tile in board) Assert.AreEqual(PieceType.NullPiece, tile.CurrentPiece.Type);
+        }
+    }
+}

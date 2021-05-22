9fb5f41c71e0d20fb3ee3e6487ff6718ecaae1af
Harry Prior <harryprior@hotmail.co.uk>
2021-05-22 17:29:21 +0100

    Update installer namespaces

--- Assets/Tests/UnitTests/PieceMoveTests/PossibleMoves/PossibleMovesTestBase.cs
+++ Assets/Tests/UnitTests/PieceMoveTests/PossibleMoves/PossibleMovesTestBase.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Bindings.Installers.BoardInstallers;
+using Bindings.Installers.InputInstallers;
+using Bindings.Installers.PieceInstallers;
 using Models.Services.Interfaces;
 using Models.Services.Moves.PossibleMoveHelpers;
 using Models.State.Board;

ba4cb9d16790b126996c61bf4f213e25ef253bc1
Harry Prior <harryprior@hotmail.co.uk>
2021-05-23 11:13:14 +0100

    Add init test setup for BoardMoveEval tests

--- Assets/Tests/UnitTests/PossibleMoves/Helpers/BoardMoveEvalTests.cs
+++ Assets/Tests/UnitTests/PossibleMoves/Helpers/BoardMoveEvalTests.cs
@@ -1,12 +1,43 @@
-using Zenject;
+using Bindings.Installers.PieceInstallers;
+using Models.Services.Interfaces;
+using Models.State.Board;
+using Models.State.Interfaces;
 using NUnit.Framework;
+using Zenject;
 
-[TestFixture]
-public class UntitledUnitTest : ZenjectUnitTestFixture
+namespace Tests.UnitTests.PossibleMoves.Helpers
 {
-    [Test]
-    public void RunTest1()
+    [TestFixture]
+    public class BoardMoveEvalTests : ZenjectUnitTestFixture
     {
-        // TODO
+        private IBoardMoveEval _boardEval;
+        public void ResolveContainers()
+        {
+            BoardEvalInstaller.Install(Container);
+        }
+
+        public void InstallBindings()
+        {
+            _boardEval = Container.Resolve<IBoardMoveEval>();
+
+        }
+        [SetUp]
+        public void Init()
+        {
+            ResolveContainers();
+            InstallBindings();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Container.UnbindAll();
+        }
+
+        [Test]
+        public void Identifies_NoPieceInTile()
+        {
+            ITile tile = new Tile(new BoardPosition(1,1));
+        }
     }
 }

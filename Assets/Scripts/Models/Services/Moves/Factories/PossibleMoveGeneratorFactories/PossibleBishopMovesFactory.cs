f73a0f1b82350bcd9c0378d7b65a0711ce23444e
Harry Prior <harryprior@hotmail.co.uk>
2021-05-28 21:31:57 +0100

    Add possible move generator factories

---
+++ Assets/Scripts/Models/Services/Moves/Factories/PossibleMoveGeneratorFactories/PossibleBishopMovesFactory.cs
@@ -0,0 +1,20 @@
+﻿using Models.Services.Moves.PossibleMoveGenerators;
+using Models.State.PieceState;
+
+namespace Models.Services.Moves.Factories.PossibleMoveGeneratorFactories
+{
+    public class PossibleBishopMovesFactory
+    {
+        private readonly PossibleBishopMoves.Factory _factory;
+
+        public PossibleBishopMovesFactory(PossibleBishopMoves.Factory factory)
+        {
+            _factory = factory;
+        }
+
+        public PossibleBishopMoves Create(PieceColour pieceColour)
+        {
+            return _factory.Create(pieceColour);
+        }
+    }
+}

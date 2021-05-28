5b014194fb1b4eed2e45ea78f3c1488deda6c2b4
Harry Prior <harryprior@hotmail.co.uk>
2021-05-28 21:31:14 +0100

    comments

--- Assets/Scripts/Controllers/PieceMovers/MoveValidator.cs
+++ Assets/Scripts/Controllers/PieceMovers/MoveValidator.cs
@@ -6,6 +6,10 @@ namespace Controllers.PieceMovers
 {
     public class MoveValidator : IMoveValidator
     {
+        /*
+         * Keep move validator taking possible move argument. This will allow it to be used in other commands, and on
+         * arbitrary board states
+         */
         public bool ValidateMove(IDictionary<BoardPosition, HashSet<BoardPosition>> possibleMoves,
             BoardPosition from, BoardPosition destination)
         {

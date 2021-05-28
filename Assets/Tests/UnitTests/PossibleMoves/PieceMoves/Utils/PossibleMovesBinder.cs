c0872418f079e79a81981addd9430e81076eaf01
Harry Prior <harryprior@hotmail.co.uk>
2021-05-28 21:33:07 +0100

    Add bindings for possible moves

--- Assets/Tests/UnitTests/PossibleMoves/PieceMoves/Utils/PossibleMovesBinder.cs
+++ Assets/Tests/UnitTests/PossibleMoves/PieceMoves/Utils/PossibleMovesBinder.cs
@@ -1,9 +1,10 @@
 ﻿using Bindings.Installers.BoardInstallers;
 using Bindings.Installers.InputInstallers;
 using Bindings.Installers.PieceInstallers;
+using Bindings.Installers.PossibleMoveInstallers;
 using Zenject;
 
-namespace Tests.UnitTests.PossibleMoves.PossibleMoves.Utils
+namespace Tests.UnitTests.PossibleMoves.PieceMoves.Utils
 {
     public static class PossibleMovesBinder
     {
@@ -14,6 +15,12 @@ namespace Tests.UnitTests.PossibleMoves.PossibleMoves.Utils
             PossibleMoveFactoryInstaller.Install(Container);
             CommandInvokerInstaller.Install(Container);
             MoveCommandInstaller.Install(Container);
+            PossibleBishopMovesInstaller.Install(Container);
+            PossibleKingMovesInstaller.Install(Container);
+            PossibleKnightMovesInstaller.Install(Container);
+            PossiblePawnMovesInstaller.Install(Container);
+            PossibleQueenMovesInstaller.Install(Container);
+            PossibleRookMovesInstaller.Install(Container);
             TileEvaluatorInstaller.Install(Container);
             BoardScannerInstaller.Install(Container);
             PositionTranslatorInstaller.Install(Container);

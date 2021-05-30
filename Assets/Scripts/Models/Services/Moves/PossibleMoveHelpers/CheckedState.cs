50c8141cebe5a65c9722c410d930b1d25de29c32
Harry Prior <harryprior@hotmail.co.uk>
2021-05-30 11:32:06 +0100

    Add another method to calculate possible king moves

--- Assets/Scripts/Models/Services/Moves/PossibleMoveHelpers/CheckedState.cs
+++ Assets/Scripts/Models/Services/Moves/PossibleMoveHelpers/CheckedState.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Models.Services.Interfaces;
 using Models.State.Board;
-using Models.State.PieceState;
 
 namespace Models.Services.Moves.PossibleMoveHelpers
 {
@@ -13,62 +13,61 @@ namespace Models.Services.Moves.PossibleMoveHelpers
         private readonly HashSet<BoardPosition> _positionsBetweenKingAndCheckPiece;
         private readonly IPossibleMoveFactory _possibleMoveFactory;
         private readonly BoardPosition _previousMove;
-        private readonly PieceColour _turn;
-        private BoardPosition _kingPosition;
 
-        public CheckedState(
-            BoardState boardState,
+        public CheckedState(BoardState boardState,
             BoardPosition previousMove,
-            PieceColour turn,
-            IPossibleMoveFactory possibleMoveFactory)
+            IPossibleMoveFactory possibleMoveFactory,
+            BoardPosition kingPosition)
         {
             _boardState = boardState;
             _previousMove = previousMove;
-            _turn = turn;
             _possibleMoveFactory = possibleMoveFactory;
 
             // check for condition and return pieces between checked king and checking piece
-            var checkedPositions = GetPositionsBetweenCheckedKing();
+            var checkedPositions = GetPositionsBetweenCheckedKing(kingPosition);
             _positionsBetweenKingAndCheckPiece = checkedPositions;
             IsTrue = checkedPositions.Any();
         }
 
         public bool IsTrue { get; }
 
+        // in the case of a double check exit out here early and return empty possible moves aside from king moves
         public IDictionary<BoardPosition, HashSet<BoardPosition>> PossibleNonKingMovesWhenInCheck(
-            IDictionary<BoardPosition, HashSet<BoardPosition>> possibleMoves)
+            IDictionary<BoardPosition, HashSet<BoardPosition>> possibleMoves, BoardPosition kingPosition)
         {
             foreach (var keyVal in possibleMoves)
             {
-                var notKingPiece = !keyVal.Key.Equals(_kingPosition);
+                var notKingPiece = !keyVal.Key.Equals(kingPosition);
                 if (notKingPiece) keyVal.Value.IntersectWith(_positionsBetweenKingAndCheckPiece);
             }
 
             return possibleMoves;
         }
 
-        private HashSet<BoardPosition> GetPositionsBetweenCheckedKing()
+        public IDictionary<BoardPosition, HashSet<BoardPosition>> PossibleKingMovesWhenInCheck(
+            IDictionary<BoardPosition, HashSet<BoardPosition>> possibleMoves, BoardPosition kingPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        // this will need to change when all non turn moves are evaluated for check, to catch discovered check, and double check
+        // if two checks are found exit early and return an empty hashset - this will intersect with all moves producing no moves
+        // hence only the king will be able to move
+        private HashSet<BoardPosition> GetPositionsBetweenCheckedKing(BoardPosition kingPosition)
         {
             var movedPiece = _boardState.Board[_previousMove.X, _previousMove.Y].CurrentPiece;
             var possibleMoves =
                 _possibleMoveFactory
                     .GetPossibleMoveGenerator(movedPiece)
                     .GetPossiblePieceMoves(_previousMove, _boardState);
-            foreach (var boardPosition in possibleMoves)
-            {
-                var pieceAtBoardPosition = _boardState.Board[boardPosition.X, boardPosition.Y].CurrentPiece;
 
-                var kingPosition = pieceAtBoardPosition.Colour != movedPiece.Colour &&
-                                   (pieceAtBoardPosition.Type == PieceType.BlackKing ||
-                                    pieceAtBoardPosition.Type == PieceType.WhiteKing);
-                if (kingPosition)
+            foreach (var boardPosition in possibleMoves)
+                if (kingPosition.Equals(boardPosition))
                 {
-                    _kingPosition = boardPosition;
                     var result = ScanPositionGenerator.GetPositionsBetween(boardPosition, _previousMove)
                         .Concat(new List<BoardPosition> {_previousMove});
                     return new HashSet<BoardPosition>(result);
                 }
-            }
 
             return new HashSet<BoardPosition>();
         }

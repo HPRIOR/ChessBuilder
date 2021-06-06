b02308320b6803f5a65fe2890ec25b249d473fd2
Harry Prior <harryprior@hotmail.co.uk>
2021-06-06 12:11:00 +0100

    Add todo

--- Assets/Tests/UnitTests/PossibleMoves/PieceMoves/Utils/PossibleMovesBinder.cs
+++ Assets/Tests/UnitTests/PossibleMoves/PieceMoves/Utils/PossibleMovesBinder.cs
@@ -1,11 +1,11 @@
 ﻿using Bindings.Installers.BoardInstallers;
-using Bindings.Installers.InputInstallers;
 using Bindings.Installers.PieceInstallers;
 using Bindings.Installers.PossibleMoveInstallers;
 using Zenject;
 
 namespace Tests.UnitTests.PossibleMoves.PieceMoves.Utils
 {
+    // TODO: refactor so each test installs only what it needs, and mocks where necessary 
     public static class PossibleMovesBinder
     {
         public static void InstallBindings(DiContainer Container)
@@ -13,8 +13,6 @@ namespace Tests.UnitTests.PossibleMoves.PieceMoves.Utils
             BoardGeneratorInstaller.Install(Container);
             PieceSpawnerInstaller.Install(Container);
             PossibleMoveFactoryInstaller.Install(Container);
-            CommandInvokerInstaller.Install(Container);
-            MoveCommandInstaller.Install(Container);
             PossibleBishopMovesInstaller.Install(Container);
             PossibleKingMovesInstaller.Install(Container);
             PossibleKnightMovesInstaller.Install(Container);

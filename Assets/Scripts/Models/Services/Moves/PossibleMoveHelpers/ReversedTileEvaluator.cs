19c9481459ab9fabf8ee35912e6d742cc2f555ba
Harry Prior <harryprior@hotmail.co.uk>
2021-06-07 17:43:14 +0100

    Add reversed tile evaluator

---
+++ Assets/Scripts/Models/Services/Moves/PossibleMoveHelpers/ReversedTileEvaluator.cs
@@ -0,0 +1,36 @@
+﻿using Models.Services.Interfaces;
+using Models.State.Board;
+using Models.State.PieceState;
+using Zenject;
+
+namespace Models.Services.Moves.PossibleMoveHelpers
+{
+    public class ReversedTileEvaluator : ITileEvaluator
+    {
+        private readonly PieceColour _pieceColour;
+
+        public ReversedTileEvaluator(PieceColour pieceColour)
+        {
+            _pieceColour = pieceColour;
+        }
+
+        public bool NoPieceIn(Tile tile)
+        {
+            return tile.CurrentPiece.Type == PieceType.NullPiece;
+        }
+
+        public bool FriendlyPieceIn(Tile tile)
+        {
+            return !(tile.CurrentPiece.Type is PieceType.NullPiece) && tile.CurrentPiece.Colour != _pieceColour;
+        }
+
+        public bool OpposingPieceIn(Tile tile)
+        {
+            return !(tile.CurrentPiece.Type is PieceType.NullPiece) && tile.CurrentPiece.Colour == _pieceColour;
+        }
+
+        public class Factory : PlaceholderFactory<PieceColour, ReversedTileEvaluator>
+        {
+        }
+    }
+}

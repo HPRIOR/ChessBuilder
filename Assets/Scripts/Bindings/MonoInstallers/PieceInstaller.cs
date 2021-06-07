88614cf56b2e00586b6bc1f216ea5149d13ce736
Harry Prior <harryprior@hotmail.co.uk>
2021-06-07 17:39:00 +0100

    Remove old factories from piece installer

--- Assets/Scripts/Bindings/MonoInstallers/PieceInstaller.cs
+++ Assets/Scripts/Bindings/MonoInstallers/PieceInstaller.cs
@@ -10,22 +10,17 @@ namespace Bindings.MonoInstallers
         {
             PieceSpawnerInstaller.Install(Container);
             PieceMoverInstaller.Install(Container);
-            PossibleMoveFactoryInstaller.Install(Container);
+            MoveGeneratorRepositoryInstaller.Install(Container);
             AllPossibleMovesGeneratorInstaller.Install(Container);
 
-            BishopTurnMovesInstaller.Install(Container);
-            KingTurnMovesInstaller.Install(Container);
-            KnightTurnMovesInstaller.Install(Container);
-            PawnTurnMovesInstaller.Install(Container);
-            QueenTurnMovesInstaller.Install(Container);
-            RookTurnMovesInstaller.Install(Container);
 
+            PossibleMovesFactoryInstaller.Install(Container);
             MoveValidatorInstaller.Install(Container);
             TileEvaluatorInstaller.Install(Container);
             PositionTranslatorInstaller.Install(Container);
             BoardScannerInstaller.Install(Container);
             BoardEvalInstaller.Install(Container);
-            NonTurnRemoverInstaller.Install(Container);
+            KingMoveFilterInstaller.Install(Container);
         }
     }
 }

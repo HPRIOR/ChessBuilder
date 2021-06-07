5d09df9dee5246e9932e2fef3d1645bedf62dc1c
Harry Prior <harryprior@hotmail.co.uk>
2021-06-07 17:35:45 +0100

    Add installer for PossibleMovesFactory.cs

---
+++ Assets/Scripts/Bindings/Installers/MoveInstallers/PossibleMovesFactoryInstaller.cs
@@ -0,0 +1,21 @@
+﻿using Models.Services.Moves.Factories.PossibleMoveGeneratorFactories;
+using Models.Services.Moves.PossibleMoveGenerators.TurnMoves;
+using Models.State.PieceState;
+using Zenject;
+
+namespace Bindings.Installers.MoveInstallers
+{
+    public class PossibleMovesFactoryInstaller : Installer<PossibleMovesFactoryInstaller>
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<PossibleMovesFactory>().AsSingle();
+            Container.BindFactory<PieceColour, QueenTurnMoves, QueenTurnMoves.Factory>().FromNew();
+            Container.BindFactory<PieceColour, RookTurnMoves, RookTurnMoves.Factory>().FromNew();
+            Container.BindFactory<PieceColour, PawnTurnMoves, PawnTurnMoves.Factory>().FromNew();
+            Container.BindFactory<PieceColour, BishopTurnMoves, BishopTurnMoves.Factory>().FromNew();
+            Container.BindFactory<PieceColour, KingTurnMoves, KingTurnMoves.Factory>().FromNew();
+            Container.BindFactory<PieceColour, KnightTurnMoves, KnightTurnMoves.Factory>().FromNew();
+        }
+    }
+}

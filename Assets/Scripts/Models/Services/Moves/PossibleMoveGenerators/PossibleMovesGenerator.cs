7c42bbd25909195f3ed560627ac3e39f3ffd18f3
Harry Prior <harryprior@hotmail.co.uk>
2021-06-10 09:27:14 +0100

    Comments

--- Assets/Scripts/Models/Services/Moves/PossibleMoveGenerators/PossibleMovesGenerator.cs
+++ Assets/Scripts/Models/Services/Moves/PossibleMoveGenerators/PossibleMovesGenerator.cs
@@ -17,7 +17,19 @@ namespace Models.Services.Moves.PossibleMoveGenerators
             _kingMoveFilter = kingMoveFilter;
         }
 
-
+        /// <summary>
+        ///     Generates all the possible moves for a given player given the state of the board.
+        /// </summary>
+        /// <remarks>
+        ///     This works by calculating all the possible moves for turn and non-turn pieces. Turn moves include opposing
+        ///     pieces, and preclude friendly pieces. Non turn Moves include both opposing pieces and friendly pieces.
+        ///     If check occurs, then only those moves between the king and the checking piece are allowed for non-king pieces
+        ///     and a move away from check for the king piece. King moves are filtered by non turn moves. This is why non-turn
+        ///     moves include friendly pieces, as this constitutes a protected piece which the king cannot take.
+        /// </remarks>
+        /// <param name="boardState"></param>
+        /// <param name="turn"></param>
+        /// <returns></returns>
         public IDictionary<BoardPosition, HashSet<BoardPosition>> GetPossibleMoves(BoardState boardState,
             PieceColour turn)
         {

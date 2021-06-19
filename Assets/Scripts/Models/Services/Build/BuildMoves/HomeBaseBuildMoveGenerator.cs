71a6edc195bc6609e656e375685130988db5a7bb
Harry Prior <harryprior@hotmail.co.uk>
2021-06-19 19:50:12 +0100

    Change signature of home base build move generator

--- Assets/Scripts/Models/Services/Build/BuildMoves/HomeBaseBuildMoveGenerator.cs
+++ Assets/Scripts/Models/Services/Build/BuildMoves/HomeBaseBuildMoveGenerator.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using Models.Services.Build.Interfaces;
-using Models.Services.Moves.MoveHelpers;
 using Models.State.Board;
+using Models.State.PieceState;
 
 namespace Models.Services.Build.BuildMoves
 {
     internal class HomeBaseBuildMoveGenerator : IBuildMoveGenerator
     {
-        public IEnumerable<Position> GetPossibleBuildMoves(BoardState currentBoardState, Turn turn) =>
+        public IEnumerable<Position> GetPossibleBuildMoves(BoardState currentBoardState, PieceColour turn) =>
             throw new NotImplementedException();
 
         private IEnumerable<Position> BlackBuildMoveGenerator(BoardState boardState) =>

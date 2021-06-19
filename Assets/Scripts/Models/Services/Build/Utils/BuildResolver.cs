665b858dbafca268c7b8d2c8fb978df9da2115a6
Harry Prior <harryprior@hotmail.co.uk>
2021-06-19 17:00:40 +0100

    Add class for resolving builds

---
+++ Assets/Scripts/Models/Services/Build/Utils/BuildResolver.cs
@@ -0,0 +1,21 @@
+﻿using Models.Services.Build.Interfaces;
+using Models.State.Board;
+using Models.State.BuildState;
+using Models.State.PieceState;
+
+namespace Models.Services.Build.Utils
+{
+    public class BuildResolver : IBuildResolver
+    {
+        public void ResolveBuild(BoardState boardState)
+        {
+            foreach (var tile in boardState.Board)
+                if (tile.BuildState.BuildingPiece.Type != PieceType.NullPiece &&
+                    tile.CurrentPiece.Type == PieceType.NullPiece)
+                {
+                    tile.CurrentPiece = tile.BuildState.BuildingPiece;
+                    tile.BuildState = new BuildState(); // reset build state
+                }
+        }
+    }
+}

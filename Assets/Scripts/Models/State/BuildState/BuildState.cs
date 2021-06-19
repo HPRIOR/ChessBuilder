63d8f0dc108ce78d0a480e130c8c535e0d3a52d0
Harry Prior <harryprior@hotmail.co.uk>
2021-06-19 17:00:19 +0100

    Add class to handle build state

---
+++ Assets/Scripts/Models/State/BuildState/BuildState.cs
@@ -0,0 +1,18 @@
+﻿using Models.State.PieceState;
+
+namespace Models.State.BuildState
+{
+    public readonly struct BuildState
+    {
+        public Piece BuildingPiece { get; }
+        public int Turns { get; }
+
+        public BuildState(int turns = 0, Piece buildingPiece = default)
+        {
+            BuildingPiece = buildingPiece;
+            Turns = turns;
+        }
+
+        public BuildState Decrement() => Turns == 0 ? this : new BuildState(Turns - 1, BuildingPiece);
+    }
+}

f39c020368e37f786aecca2692632cead8d77988
Harry Prior <harryprior@hotmail.co.uk>
2021-06-19 19:24:15 +0100

    Update Piece mover to include resolution and decrementing of build state

--- Assets/Scripts/Controllers/PieceMovers/PieceMover.cs
+++ Assets/Scripts/Controllers/PieceMovers/PieceMover.cs
@@ -1,4 +1,5 @@
-﻿using Models.Services.Interfaces;
+﻿using Models.Services.Build.Interfaces;
+using Models.Services.Interfaces;
 using Models.State.Board;
 using Models.State.PieceState;
 
@@ -6,6 +7,13 @@ namespace Controllers.PieceMovers
 {
     public class PieceMover : IPieceMover
     {
+        private readonly IBuildResolver _buildResolver;
+
+        public PieceMover(IBuildResolver buildResolver)
+        {
+            _buildResolver = buildResolver;
+        }
+
         public BoardState GenerateNewBoardState(BoardState originalBoardState, Position from,
             Position destination)
         {
@@ -18,6 +26,8 @@ namespace Controllers.PieceMovers
             // swap pieces
             destinationTile.CurrentPiece = originalBoardState.Board[from.X, from.Y].CurrentPiece;
             fromTile.CurrentPiece = new Piece(PieceType.NullPiece);
+            _buildResolver.ResolveBuild(newBoardState);
+
             return newBoardState;
         }
     }
--- Assets/Tests/UnitTests/Controllers/PieceMovers/PieceMoverTest.cs
+++ Assets/Tests/UnitTests/Controllers/PieceMovers/PieceMoverTest.cs
@@ -1,7 +1,9 @@
 using Bindings.Installers.BoardInstallers;
+using Bindings.Installers.MoveInstallers;
 using Bindings.Installers.PieceInstallers;
 using Models.Services.Interfaces;
 using Models.State.Board;
+using Models.State.BuildState;
 using Models.State.PieceState;
 using NUnit.Framework;
 using Zenject;
@@ -29,6 +31,7 @@ namespace Tests.UnitTests.Controllers.PieceMovers
         private void InstallBindings()
         {
             PieceMoverInstaller.Install(Container);
+            BuildResolverInstaller.Install(Container);
             BoardGeneratorInstaller.Install(Container);
         }
 
@@ -99,6 +102,121 @@ namespace Tests.UnitTests.Controllers.PieceMovers
 
             var newPiece = newState.Board[2, 2].CurrentPiece;
             Assert.AreNotSame(oldPiece, newPiece);
+        }
+
+
+        [Test]
+        public void BuildStateIsDecremented()
+        {
+            var board = _boardGenerator.GenerateBoard();
+            board[1, 1].CurrentPiece = new Piece(PieceType.BlackKing);
+            board[5, 5].BuildState = new BuildState(5, new Piece(PieceType.BlackKnight));
+            var boardState = new BoardState(board);
+
+            var newState =
+                _pieceMover.GenerateNewBoardState(boardState, new Position(1, 1), new Position(2, 2));
+
+            var buildState = newState.Board[5, 5].BuildState;
+
+            Assert.That(buildState.Turns, Is.EqualTo(4));
+        }
+
+
+        [Test]
+        public void BuildStateIsResolved()
+        {
+            var board = _boardGenerator.GenerateBoard();
+            board[1, 1].CurrentPiece = new Piece(PieceType.BlackKing);
+            board[5, 5].BuildState = new BuildState(1, new Piece(PieceType.BlackKnight));
+            var boardState = new BoardState(board);
+
+            var newState =
+                _pieceMover.GenerateNewBoardState(boardState, new Position(1, 1), new Position(2, 2));
+
+
+            Assert.That(newState.Board[5, 5].CurrentPiece.Type, Is.EqualTo(PieceType.BlackKnight));
+        }
+
+        [Test]
+        public void BuildStateIsResetAfterResolved()
+        {
+            var board = _boardGenerator.GenerateBoard();
+            board[1, 1].CurrentPiece = new Piece(PieceType.BlackKing);
+            board[5, 5].BuildState = new BuildState(1, new Piece(PieceType.BlackKnight));
+            var boardState = new BoardState(board);
+
+            var newState =
+                _pieceMover.GenerateNewBoardState(boardState, new Position(1, 1), new Position(2, 2));
+
+
+            Assert.That(newState.Board[5, 5].BuildState, Is.EqualTo(new BuildState()));
+        }
+
+
+        [Test]
+        public void WhenBuildIsBlocked_NoPieceIsBuilt()
+        {
+            var board = _boardGenerator.GenerateBoard();
+            board[5, 5].CurrentPiece = new Piece(PieceType.BlackKing);
+            board[5, 5].BuildState = new BuildState(1, new Piece(PieceType.BlackKnight));
+            var boardState = new BoardState(board);
+
+            var newState =
+                _pieceMover.GenerateNewBoardState(boardState, new Position(1, 1), new Position(2, 2));
+
+            Assert.That(newState.Board[5, 5].CurrentPiece.Type, Is.EqualTo(PieceType.BlackKing));
+        }
+
+
+        [Test]
+        public void WhenBuildIsBlocked_BuildStateIsDecremented()
+        {
+            var board = _boardGenerator.GenerateBoard();
+            board[5, 5].CurrentPiece = new Piece(PieceType.BlackKing);
+            board[5, 5].BuildState = new BuildState(1, new Piece(PieceType.BlackKnight));
+            var boardState = new BoardState(board);
+
+            var newState =
+                _pieceMover.GenerateNewBoardState(boardState, new Position(1, 1), new Position(2, 2));
+
+            Assert.That(newState.Board[5, 5].BuildState,
+                Is.EqualTo(new BuildState(0, new Piece(PieceType.BlackKnight))));
+        }
+
+        [Test]
+        public void BuildIsUnblocked_ByMovingPiece()
+        {
+            var board = _boardGenerator.GenerateBoard();
+            board[5, 5].CurrentPiece = new Piece(PieceType.BlackKing);
+            board[5, 5].BuildState = new BuildState(1, new Piece(PieceType.BlackKnight));
+            var boardState = new BoardState(board);
+
+            // update state but don't move blocking piece
+            var firstState =
+                _pieceMover.GenerateNewBoardState(boardState, new Position(1, 1), new Position(2, 2));
+
+
+            Assert.That(firstState.Board[5, 5].CurrentPiece.Type, Is.EqualTo(PieceType.BlackKing));
+
+            // move blocking piece
+            var secondState =
+                _pieceMover.GenerateNewBoardState(boardState, new Position(5, 5), new Position(6, 6));
+
+            Assert.That(secondState.Board[5, 5].CurrentPiece.Type, Is.EqualTo(PieceType.BlackKnight));
+        }
+
+        [Test]
+        public void BuildIsBlocked_ByMovingPiece()
+        {
+            var board = _boardGenerator.GenerateBoard();
+            board[4, 4].CurrentPiece = new Piece(PieceType.BlackKing);
+            board[5, 5].BuildState = new BuildState(1, new Piece(PieceType.BlackKnight));
+            var boardState = new BoardState(board);
+
+            var firstState =
+                _pieceMover.GenerateNewBoardState(boardState, new Position(4, 4), new Position(5, 5));
+
+            Assert.That(firstState.Board[5, 5].CurrentPiece.Type, Is.EqualTo(PieceType.BlackKing));
         }
     }
 }

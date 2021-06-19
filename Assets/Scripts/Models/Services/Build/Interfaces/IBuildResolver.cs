c441d6de22ca07b718fa11ddc2754a996cef1cce
Harry Prior <harryprior@hotmail.co.uk>
2021-06-19 17:00:54 +0100

    Add interface for resolving builds

---
+++ Assets/Scripts/Models/Services/Build/Interfaces/IBuildResolver.cs
@@ -0,0 +1,9 @@
+﻿using Models.State.Board;
+
+namespace Models.Services.Build.Interfaces
+{
+    public interface IBuildResolver
+    {
+        public void ResolveBuild(BoardState boardState);
+    }
+}

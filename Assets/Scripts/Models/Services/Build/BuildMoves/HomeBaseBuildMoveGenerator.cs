46a2bb696998394e77f6a5ccc8a8c9f3380d40a4
Harry Prior <harryprior@hotmail.co.uk>
2021-06-19 14:01:17 +0100

    Add implementation for possible build moves generator

---
+++ Assets/Scripts/Models/Services/Build/BuildMoves/HomeBaseBuildMoveGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Models.Services.Build.Interfaces;
+using Models.Services.Moves.MoveHelpers;
+using Models.State.Board;
+
+namespace Models.Services.Build.BuildMoves
+{
+    internal class HomeBaseBuildMoveGenerator : IBuildMoveGenerator
+    {
+        public IEnumerable<Position> GetPossibleBuildMoves(BoardState currentBoardState, Turn turn) =>
+            throw new NotImplementedException();
+
+        private IEnumerable<Position> BlackBuildMoveGenerator() => throw new NotImplementedException();
+    }
+}

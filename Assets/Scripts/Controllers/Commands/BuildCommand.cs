369788edc0d6db76056e211f931ee7e1afe1c9d6
Harry Prior <harryprior@hotmail.co.uk>
2021-06-19 14:02:04 +0100

    Add initial build move

---
+++ Assets/Scripts/Controllers/Commands/BuildCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Controllers.Interfaces;
+using Models.State.Board;
+using Models.State.PieceState;
+
+namespace Controllers.Commands
+{
+    public class BuildCommand : ICommand
+    {
+        public BuildCommand(Position at, Piece piece)
+        {
+        }
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Undo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsValid() => throw new NotImplementedException();
+    }
+}

5c07de09e91d3868c515f137200ac99896cc90e4
Harry Prior <harryprior@hotmail.co.uk>
2021-06-20 10:39:52 +0100

    logic and tests for Builder class

--- Assets/Tests/UnitTests/PossibleMoves/BuildMoves/BuilderTests.cs
+++ Assets/Tests/UnitTests/PossibleMoves/BuildMoves/BuilderTests.cs
@@ -33,6 +33,7 @@ namespace Tests.UnitTests.PossibleMoves.BuildMoves
         {
             BuilderInstaller.Install(Container);
             BoardGeneratorInstaller.Install(Container);
+            BuildResolverInstaller.Install(Container);
         }
 
         private void ResolveContainer()
@@ -52,13 +53,77 @@ namespace Tests.UnitTests.PossibleMoves.BuildMoves
 
 
         [Test]
-        public void CreatesNewBoardState_WithBoardStateAtPosition()
+        public void CreatesNewBoardState_WithBuilddStateAtPosition()
         {
             var boardState = new BoardState();
             var newBoardState = _builder.GenerateNewBoardState(boardState, new Position(5, 5), PieceType.BlackKnight);
 
             Assert.That(newBoardState.Board[5, 5].BuildState,
                 Is.EqualTo(new BuildState(3, PieceType.BlackKnight)));
+        }
+
+        [Test]
+        public void OverwritesPreviousBuild()
+        {
+            var boardState = new BoardState();
+            boardState.Board[5, 5].BuildState = new BuildState(PieceType.BlackPawn);
+            var newBoardState = _builder.GenerateNewBoardState(boardState, new Position(5, 5), PieceType.BlackKnight);
+
+            Assert.That(newBoardState.Board[5, 5].BuildState,
+                Is.EqualTo(new BuildState(3, PieceType.BlackKnight)));
+        }
+
+        [Test]
+        public void DecrementsBuildsOnBoard()
+        {
+            var boardState = new BoardState();
+            boardState.Board[5, 5].BuildState = new BuildState(PieceType.WhiteRook);
+
+            var newBoardState = _builder.GenerateNewBoardState(boardState, new Position(6, 6), PieceType.BlackKnight);
+
+            Assert.That(newBoardState.Board[5, 5].BuildState,
+                Is.EqualTo(new BuildState(4, PieceType.WhiteRook)));
+        }
+
+
+        [Test]
+        public void ResolvesBuildsOnBoard()
+        {
+            var boardState = new BoardState();
+            boardState.Board[5, 5].BuildState = new BuildState(PieceType.BlackPawn);
+
+            var newBoardState = _builder.GenerateNewBoardState(boardState, new Position(6, 6), PieceType.BlackKnight);
+
+            Assert.That(newBoardState.Board[5, 5].CurrentPiece.Type,
+                Is.EqualTo(PieceType.BlackPawn));
+        }
+
+
+        [Test]
+        public void ResolvesBuildsOnBoard_AndResetsBuild()
+        {
+            var boardState = new BoardState();
+            boardState.Board[5, 5].BuildState = new BuildState(PieceType.BlackPawn);
+
+            var newBoardState = _builder.GenerateNewBoardState(boardState, new Position(6, 6), PieceType.BlackKnight);
+
+            Assert.That(newBoardState.Board[5, 5].BuildState,
+                Is.EqualTo(new BuildState()));
+        }
+
+
+        [Test]
+        public void OverwritesBuild_DueNextTurn()
+        {
+            var boardState = new BoardState();
+            boardState.Board[5, 5].BuildState = new BuildState(PieceType.BlackPawn);
+
+            var newBoardState = _builder.GenerateNewBoardState(boardState, new Position(5, 5), PieceType.BlackKnight);
+
+            Assert.That(newBoardState.Board[5, 5].BuildState,
+                Is.EqualTo(new BuildState(3, PieceType.BlackKnight)));
+            Assert.That(newBoardState.Board[5, 5].CurrentPiece.Type,
+                Is.EqualTo(PieceType.NullPiece));
         }
     }
 }

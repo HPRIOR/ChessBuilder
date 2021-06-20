1c2002d7ccf29e39cb51c9fe9db5e0e8f731e031
Harry Prior <harryprior@hotmail.co.uk>
2021-06-20 11:46:30 +0100

    Add BuildPoints Static class as single point for getting points for pieces

--- Assets/Scripts/Models/State/BuildState/BuildState.cs
+++ Assets/Scripts/Models/State/BuildState/BuildState.cs
@@ -1,27 +1,9 @@
-﻿using System.Collections.Generic;
-using Models.State.PieceState;
+﻿using Models.State.PieceState;
 
 namespace Models.State.BuildState
 {
     public readonly struct BuildState
     {
-        private static readonly IDictionary<PieceType, int> PieceCost = new Dictionary<PieceType, int>
-        {
-            {PieceType.NullPiece, 0},
-            {PieceType.BlackKing, 0},
-            {PieceType.WhiteKing, 0},
-            {PieceType.BlackPawn, 1},
-            {PieceType.WhitePawn, 1},
-            {PieceType.BlackBishop, 3},
-            {PieceType.WhiteBishop, 3},
-            {PieceType.BlackKnight, 3},
-            {PieceType.WhiteKnight, 3},
-            {PieceType.BlackRook, 5},
-            {PieceType.WhiteRook, 5},
-            {PieceType.BlackQueen, 9},
-            {PieceType.WhiteQueen, 9}
-        };
-
         public PieceType BuildingPiece { get; }
         public int Turns { get; }
 
@@ -36,7 +18,7 @@ namespace Models.State.BuildState
         public BuildState(PieceType pieceType)
         {
             BuildingPiece = pieceType;
-            Turns = PieceCost[pieceType];
+            Turns = BuildPoints.PieceCost[pieceType];
         }
 
 

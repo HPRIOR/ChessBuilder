fafff6cff2b47b102127361804d3db0927378725
Harry Prior <harryprior@hotmail.co.uk>
2021-06-26 12:44:27 +0100

    Change name of PieceSpawner to PieceFactory

---
+++ Assets/Scripts/Models/Services/Interfaces/IPieceFactory.cs
@@ -0,0 +1,11 @@
+﻿using Models.State.Board;
+using Models.State.PieceState;
+using View.Utils;
+
+namespace Models.Services.Interfaces
+{
+    public interface IPieceFactory
+    {
+        PieceSpawner CreatePiece(PieceType pieceType, Position position);
+    }
+}

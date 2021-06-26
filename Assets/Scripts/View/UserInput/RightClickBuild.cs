eef0f125d7adb8778c9e8ead6440c3307d4929a0
Harry Prior <harryprior@hotmail.co.uk>
2021-06-26 12:47:05 +0100

    Add class for right click building on game object click

---
+++ Assets/Scripts/View/UserInput/RightClickBuild.cs
@@ -0,0 +1,35 @@
+﻿using Controllers.Factories;
+using Controllers.Interfaces;
+using Models.State.PieceState;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using View.Utils;
+using Zenject;
+
+namespace View.UserInput
+{
+    public class RightClickBuild : MonoBehaviour, IPointerClickHandler
+    {
+        private static ICommandInvoker _commandInvoker;
+        private static BuildCommandFactory _buildCommandFactory;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (eventData.button == PointerEventData.InputButton.Right)
+            {
+                var nearestPos = NearestBoardPosFinder.GetNearestBoardPosition(eventData.position);
+                _commandInvoker.AddCommand(
+                    _buildCommandFactory.Create(nearestPos, PieceType.BlackPawn)
+                );
+            }
+        }
+
+        [Inject]
+        public void Construct(ICommandInvoker commandInvoker, BuildCommandFactory buildCommandFactory)
+        {
+            _commandInvoker = commandInvoker;
+            Debug.Log(_commandInvoker);
+            _buildCommandFactory = buildCommandFactory;
+        }
+    }
+}

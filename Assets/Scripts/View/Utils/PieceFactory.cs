f13d0fdfa33762f20b356a714b8cadf01bf97daf
Harry Prior <harryprior@hotmail.co.uk>
2021-06-26 12:45:29 +0100

    Change name to PieceFactory.cs

---
+++ Assets/Scripts/View/Utils/PieceFactory.cs
@@ -0,0 +1,22 @@
+﻿using Models.Services.Interfaces;
+using Models.State.Board;
+using Models.State.PieceState;
+
+namespace View.Utils
+{
+    public class PieceFactory : IPieceFactory
+    {
+        private readonly PieceSpawner.Factory _pieceFactory;
+
+        public PieceFactory(PieceSpawner.Factory pieceFactory)
+        {
+            _pieceFactory = pieceFactory;
+        }
+
+        public PieceSpawner CreatePiece(PieceType pieceType, Position position)
+        {
+            var piece = _pieceFactory.Create(new PieceInfo(pieceType), position);
+            return piece;
+        }
+    }
+}

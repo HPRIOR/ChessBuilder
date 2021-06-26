906e591eab5990571cd50a3a4557bcd0741218fd
Harry Prior <harryprior@hotmail.co.uk>
2021-06-26 20:26:04 +0100

    Add inital logic for build renderer

--- Assets/Scripts/View/Renderers/BuildRenderer.cs
+++ Assets/Scripts/View/Renderers/BuildRenderer.cs
@@ -1,5 +1,8 @@
 ﻿using Models.State.Board;
+using Models.State.BuildState;
+using Models.State.PieceState;
 using View.Interfaces;
+using View.Utils;
 
 namespace View.Renderers
 {
@@ -7,6 +10,20 @@ namespace View.Renderers
     {
         public void Render(BoardState previousState, BoardState newState)
         {
+            GameObjectDestroyer.DestroyChildrenOfObjectWith("BuildingPieces");
+            var board = newState.Board;
+            foreach (var tile in board)
+            {
+                var isBuilding = tile.BuildState.BuildingPiece != PieceType.NullPiece;
+                if (isBuilding) RenderBuild(tile.BuildState, tile.Position);
+            }
+        }
+
+        private void RenderBuild(BuildState buildState, Position position)
+        {
+            // get piece info from piece type
+            // use sprite from sprite path to render new sprite 
+            // change sprite position tile position
         }
     }
 }

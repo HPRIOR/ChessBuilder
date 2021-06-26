cb9d899260118c693def1697fe4a01a5df626420
Harry Prior <harryprior@hotmail.co.uk>
2021-06-26 12:46:32 +0100

    Extract utility lass to get nearest board position

---
+++ Assets/Scripts/View/Utils/NearestBoardPosFinder.cs
@@ -0,0 +1,19 @@
+﻿using Models.State.Board;
+using UnityEngine;
+
+namespace View.Utils
+{
+    public static class NearestBoardPosFinder
+    {
+        public static Position GetNearestBoardPosition(Vector2 position) =>
+            new Position(ConvertAxisToNearestBoardIndex(position.x),
+                ConvertAxisToNearestBoardIndex(position.y));
+
+        private static int ConvertAxisToNearestBoardIndex(float axis)
+        {
+            if (axis > 7.5) return 7;
+            if (axis < 0.5) return 0;
+            return (int) axis;
+        }
+    }
+}

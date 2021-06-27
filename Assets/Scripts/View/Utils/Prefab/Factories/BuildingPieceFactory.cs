a68115a2a4da56ad8ac44a6fcc725b3dc5ffd699
Harry Prior <harryprior@hotmail.co.uk>
2021-06-27 11:43:51 +0100

    Add factory for buildinePiece

---
+++ Assets/Scripts/View/Utils/Prefab/Factories/BuildingPieceFactory.cs
@@ -0,0 +1,23 @@
+﻿using Models.State.Board;
+using Models.State.BuildState;
+using Models.State.PieceState;
+using View.Utils.Prefab.Spawners;
+
+namespace View.Utils.Prefab.Factories
+{
+    public class BuildingPieceFactory
+    {
+        private readonly BuildingPieceSpawner.Factory _pieceFactory;
+
+        public BuildingPieceFactory(BuildingPieceSpawner.Factory pieceFactory)
+        {
+            _pieceFactory = pieceFactory;
+        }
+
+        public BuildingPieceSpawner CreatePiece(PieceType pieceType, Position position, BuildState buildState)
+        {
+            var piece = _pieceFactory.Create(new PieceRenderInfo(pieceType), position, buildState);
+            return piece;
+        }
+    }
+}

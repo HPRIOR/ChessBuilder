89f3b6161b1749f56241a3c3c0ad68163904b8cb
Harry Prior <harryprior@hotmail.co.uk>
2021-06-27 10:24:41 +0100

    make get component produce local variable

--- Assets/Scripts/View/Utils/PieceSpawner.cs
+++ Assets/Scripts/View/Utils/PieceSpawner.cs
@@ -8,15 +8,14 @@ namespace View.Utils
 {
     public class PieceSpawner : MonoBehaviour
     {
-        private SpriteRenderer _spriteRenderer;
         public Position Position { get; private set; }
         public IPieceInfo Info { get; private set; }
 
         private void Start()
         {
-            _spriteRenderer = GetComponent<SpriteRenderer>();
+            var spriteRenderer = GetComponent<SpriteRenderer>();
             if (Info.SpriteAssetPath != "")
-                _spriteRenderer.sprite = AssetDatabase.LoadAssetAtPath<Sprite>(Info.SpriteAssetPath);
+                spriteRenderer.sprite = AssetDatabase.LoadAssetAtPath<Sprite>(Info.SpriteAssetPath);
 
             // set parent object and piece position
             GameObject pieceGameObject;

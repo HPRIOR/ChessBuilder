ee82b782b872bdd56ac30c26caa40059a50d6a91
Harry Prior <harryprior@hotmail.co.uk>
2021-06-27 13:49:34 +0100

    make pawn placeholder build piece

--- Assets/Scripts/View/UserInput/RightClickBuild.cs
+++ Assets/Scripts/View/UserInput/RightClickBuild.cs
@@ -22,7 +22,7 @@ namespace View.UserInput
                 var nearestPos =
                     NearestBoardPosFinder.GetNearestBoardPosition(eventData.pointerCurrentRaycast.worldPosition);
                 _commandInvoker.AddCommand(
-                    _buildCommandFactory.Create(nearestPos, PieceType.BlackPawn)
+                    _buildCommandFactory.Create(nearestPos, PieceType.WhitePawn)
                 );
             }
         }

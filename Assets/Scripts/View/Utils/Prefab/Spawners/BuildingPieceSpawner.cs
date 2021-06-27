1e10f7ed0358b4e63a37ed8317491ce6caada725
Harry Prior <harryprior@hotmail.co.uk>
2021-06-27 11:44:18 +0100

    Add spawner for building piece

---
+++ Assets/Scripts/View/Utils/Prefab/Spawners/BuildingPieceSpawner.cs
@@ -0,0 +1,31 @@
+﻿using Models.State.Board;
+using Models.State.BuildState;
+using Models.State.Interfaces;
+using UnityEngine;
+using Zenject;
+
+namespace View.Utils.Prefab.Spawners
+{
+    public class BuildingPieceSpawner : MonoBehaviour
+    {
+        private BuildState _buildState;
+        private Position _position;
+        private IPieceRenderInfo _renderInfo;
+
+        public void Start()
+        {
+        }
+
+        [Inject]
+        public void Construct(IPieceRenderInfo pieceRenderInfo, Position position, BuildState buildState)
+        {
+            _renderInfo = pieceRenderInfo;
+            _position = position;
+            _buildState = buildState;
+        }
+
+        public class Factory : PlaceholderFactory<IPieceRenderInfo, Position, BuildState, BuildingPieceSpawner>
+        {
+        }
+    }
+}

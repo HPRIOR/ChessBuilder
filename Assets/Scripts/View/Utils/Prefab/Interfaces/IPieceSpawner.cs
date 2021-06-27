28c21f992500d494ef280553d0f0b2fd4fe09684
Harry Prior <harryprior@hotmail.co.uk>
2021-06-27 11:44:04 +0100

    Add interface for PieceSpawner

---
+++ Assets/Scripts/View/Utils/Prefab/Interfaces/IPieceSpawner.cs
@@ -0,0 +1,11 @@
+﻿using Models.State.Board;
+using Models.State.Interfaces;
+
+namespace View.Utils.Prefab.Interfaces
+{
+    public interface IPieceSpawner
+    {
+        public Position Position { get; }
+        public IPieceRenderInfo RenderInfo { get; }
+    }
+}

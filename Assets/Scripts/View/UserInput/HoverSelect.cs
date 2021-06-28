0eaaea0b0ecaa789581d39b11e9e79f8f570d29d
Harry Prior <harryprior@hotmail.co.uk>
2021-06-28 16:59:53 +0100

    meta

---
+++ Assets/Scripts/View/UserInput/HoverSelect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace View.UserInput
+{
+    public class HoverSelect : MonoBehaviour, IPointerClickHandler
+    {
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Debug.Log("log");
+            var image = GetComponent<Image>();
+        }
+    }
+}

ec455dcca183ead49c2ee546f30a9fa0e6d8cb70
Harry Prior <harryprior@hotmail.co.uk>
2021-07-11 11:23:22 +0100

    Move to string from controller to state

--- Assets/Scripts/Models/Services/Game/Implementations/GameStateController.cs
+++ Assets/Scripts/Models/Services/Game/Implementations/GameStateController.cs
@@ -43,27 +43,5 @@ namespace Models.Services.Game.Implementations
         public event Action<BoardState, BoardState> GameStateChangeEvent;
 
         private PieceColour NextTurn() => Turn == PieceColour.White ? PieceColour.Black : PieceColour.White;
-
-        public override string ToString() => "";
-        // var stringBuilder = new StringBuilder();
-        // stringBuilder.Append("Black state: \n");
-        // stringBuilder.Append($"     Build Points: {BlackState.BuildPoints} \n");
-        // stringBuilder.Append("White state: \n");
-        // stringBuilder.Append($"     Build Points: {WhiteState.BuildPoints} \n");
-        // stringBuilder.Append("Possible Moves: \n");
-        // PossiblePieceMoves.Keys.ToList().ForEach(piecePosition =>
-        // {
-        //     stringBuilder.Append(
-        //         $"     {CurrentBoardState.Board[piecePosition.X, piecePosition.Y].CurrentPiece.Type}: \n       ");
-        //     PossiblePieceMoves[piecePosition].ToList().ForEach(move => stringBuilder.Append($"({move}), "));
-        // });
-        // stringBuilder.Append("\n");
-        // stringBuilder.Append("Possible Build Pieces: \n     ");
-        // PossibleBuildMoves.BuildPieces.ToList().ForEach(piece => stringBuilder.Append($"{piece}, "));
-        // stringBuilder.Append("\n");
-        // stringBuilder.Append("Possible Build Positions: \n     ");
-        // PossibleBuildMoves.BuildPositions.OrderBy(p => p.Y).ToList()
-        //     .ForEach(position => stringBuilder.Append($"({position}), "));
-        // return stringBuilder.ToString();
     }
 }
--- Assets/Scripts/Models/State/GameState/GameState.cs
+++ Assets/Scripts/Models/State/GameState/GameState.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
 using Models.State.Board;
 using Models.State.BuildState;
 
@@ -27,5 +29,29 @@ namespace Models.State.GameState
         public PlayerState.PlayerState WhiteState { get; }
         public ImmutableDictionary<Position, ImmutableHashSet<Position>> PossiblePieceMoves { get; }
         public BuildMoves PossibleBuildMoves { get; }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("Black state: \n");
+            stringBuilder.Append($"     Build Points: {BlackState.BuildPoints} \n");
+            stringBuilder.Append("White state: \n");
+            stringBuilder.Append($"     Build Points: {WhiteState.BuildPoints} \n");
+            stringBuilder.Append("Possible Moves: \n");
+            PossiblePieceMoves.Keys.ToList().ForEach(piecePosition =>
+            {
+                stringBuilder.Append(
+                    $"     {BoardState.Board[piecePosition.X, piecePosition.Y].CurrentPiece.Type}: \n       ");
+                PossiblePieceMoves[piecePosition].ToList().ForEach(move => stringBuilder.Append($"({move}), "));
+            });
+            stringBuilder.Append("\n");
+            stringBuilder.Append("Possible Build Pieces: \n     ");
+            PossibleBuildMoves.BuildPieces.ToList().ForEach(piece => stringBuilder.Append($"{piece}, "));
+            stringBuilder.Append("\n");
+            stringBuilder.Append("Possible Build Positions: \n     ");
+            PossibleBuildMoves.BuildPositions.OrderBy(p => p.Y).ToList()
+                .ForEach(position => stringBuilder.Append($"({position}), "));
+            return stringBuilder.ToString();
+        }
     }
 }

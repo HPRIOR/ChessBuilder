53fdab32738834fb73d30afb92f9b266f87809a1
Harry Prior <harryprior@hotmail.co.uk>
2021-07-12 22:04:11 +0100

    Add logic for move generation

--- Assets/Scripts/Models/Services/AI/Implementations/AiMoveCommandGenerator.cs
+++ Assets/Scripts/Models/Services/AI/Implementations/AiMoveCommandGenerator.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using Models.Services.AI.Interfaces;
+using Models.Services.Build.Interfaces;
+using Models.Services.Game.Interfaces;
+using Models.Services.Interfaces;
 using Models.State.Board;
 using Models.State.BuildState;
 using Models.State.GameState;
@@ -9,25 +13,50 @@ using Models.State.PieceState;
 
 namespace Models.Services.AI
 {
-    public class AiMoveCommandGenerator: IAiCommandGenerator
+    public class AiMoveCommandGenerator : IAiCommandGenerator
     {
-        public IEnumerable<Func<BoardState, PieceColour, GameState>> GenerateCommands(GameState gameState)
+        private readonly IPieceMover _pieceMover;
+        private readonly IBuilder _builder;
+        private readonly IGameStateUpdater _gameStateUpdater;
+
+        public AiMoveCommandGenerator(IPieceMover pieceMover, IGameStateUpdater gameStateUpdater, IBuilder builder)
         {
-            throw new NotImplementedException();
+            _pieceMover = pieceMover;
+            _gameStateUpdater = gameStateUpdater;
+            _builder = builder;
         }
 
-        private IEnumerable<Func<BoardState, PieceColour, GameState>> GetMoveCommands(ImmutableDictionary<Position, ImmutableHashSet<Position>> moves)
-        {
-            throw new NotImplementedException();
-        }
-        
-        
-        private IEnumerable<Func<BoardState, PieceColour, GameState>> GetBuildCommands(BuildMoves builds)
-        {
-            throw new NotImplementedException();
-        }
+        public IEnumerable<Func<BoardState, PieceColour, GameState>> GenerateCommands(GameState gameState) =>
+            GetBuildCommands(gameState.PossibleBuildMoves)
+                .Concat(GetMoveCommands(gameState.PossiblePieceMoves));
 
+        private IEnumerable<Func<BoardState, PieceColour, GameState>> GetMoveCommands(
+            ImmutableDictionary<Position, ImmutableHashSet<Position>> moves
+        ) =>
+            moves.SelectMany(moveSet =>
+                moveSet.Value.Select(move =>
+                {
+                    Func<BoardState, PieceColour, GameState> command = (boardState, turn) =>
+                    {
+                        var newBoardState = _pieceMover.GenerateNewBoardState(boardState, moveSet.Key, move);
+                        var nextTurn = turn == PieceColour.Black ? PieceColour.White : PieceColour.Black;
+                        return _gameStateUpdater.UpdateGameState(newBoardState, nextTurn);
+                    };
+                    return command;
+                }));
+
+
+        private IEnumerable<Func<BoardState, PieceColour, GameState>> GetBuildCommands(BuildMoves builds) =>
+            builds.BuildPositions.SelectMany(position =>
+                builds.BuildPieces.Select(piece =>
+                {
+                    Func<BoardState, PieceColour, GameState> command = (boardState, turn) =>
+                    {
+                        var newBoardState = _builder.GenerateNewBoardState(boardState, position, piece);
+                        var nextTurn = turn == PieceColour.Black ? PieceColour.White : PieceColour.Black;
+                        return _gameStateUpdater.UpdateGameState(newBoardState, nextTurn);
+                    };
+                    return command;
+                }));
     }
-    
-    
 }

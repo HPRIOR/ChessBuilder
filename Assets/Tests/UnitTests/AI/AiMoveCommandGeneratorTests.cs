41878998991a4f44349812c0634515c681c8d4a1
Harry Prior <harryprior@hotmail.co.uk>
2021-07-13 20:39:05 +0100

    cleanup code and add tests for build move

--- Assets/Tests/UnitTests/AI/AiMoveCommandGeneratorTests.cs
+++ Assets/Tests/UnitTests/AI/AiMoveCommandGeneratorTests.cs
@@ -63,75 +63,101 @@ namespace Tests.UnitTests.AI
         {
             _aiMoveCommandGenerator = Container.Resolve<IAiCommandGenerator>();
             _boardGenerator = Container.Resolve<IBoardGenerator>();
-        }   
+        }
 
         [Test]
         public void GeneratesCorrectNumberOfCommands()
         {
-
             var board = _boardGenerator.GenerateBoard();
             board[2, 3].CurrentPiece = new Piece(PieceType.BlackPawn);
             var boardState = new BoardState(board);
-            
+
             var moves = new Dictionary<Position, ImmutableHashSet<Position>>
             {
-                {new Position(2, 2), new HashSet<Position> {new Position(2,1)}.ToImmutableHashSet()}
+                {new Position(2, 2), new HashSet<Position> {new Position(2, 1)}.ToImmutableHashSet()}
             }.ToImmutableDictionary();
-            
+
             var buildMoves = new BuildMoves(
-                new HashSet<Position>{new Position(1,1)}.ToImmutableHashSet(),
-                new HashSet<PieceType>{PieceType.BlackPawn}.ToImmutableHashSet()
-                );
+                new HashSet<Position> {new Position(1, 1)}.ToImmutableHashSet(),
+                new HashSet<PieceType> {PieceType.BlackPawn}.ToImmutableHashSet()
+            );
 
             var gameState = new GameState(
-                false, 
-                false, 
-                new PlayerState(10), 
+                false,
+                false,
+                new PlayerState(10),
                 new PlayerState(10),
                 moves,
-                buildMoves, 
+                buildMoves,
                 boardState
             );
-           var commands =  _aiMoveCommandGenerator.GenerateCommands(gameState);
-           Assert.That(commands.Count(), Is.EqualTo(2));
-            
+            var commands = _aiMoveCommandGenerator.GenerateCommands(gameState);
+            Assert.That(commands.Count(), Is.EqualTo(2));
         }
-        
+
 
         [Test]
-        public void GeneratesCommands_WithCorrectGameStateOutput()
+        public void GeneratesCommands_WithCorrectPieceMoveOutput()
         {
-
             var board = _boardGenerator.GenerateBoard();
             board[2, 2].CurrentPiece = new Piece(PieceType.BlackPawn);
             var boardState = new BoardState(board);
-            
+
             var moves = new Dictionary<Position, ImmutableHashSet<Position>>
             {
-                {new Position(2, 2), new HashSet<Position> {new Position(2,1)}.ToImmutableHashSet()}
+                {new Position(2, 2), new HashSet<Position> {new Position(2, 1)}.ToImmutableHashSet()}
             }.ToImmutableDictionary();
-            
+
             var buildMoves = new BuildMoves(
                 new HashSet<Position>().ToImmutableHashSet(),
                 new HashSet<PieceType>().ToImmutableHashSet()
-                );
+            );
 
             var gameState = new GameState(
-                false, 
-                false, 
-                new PlayerState(10), 
+                false,
+                false,
+                new PlayerState(10),
                 new PlayerState(10),
                 moves,
-                buildMoves, 
+                buildMoves,
                 boardState
             );
-           var commands =  _aiMoveCommandGenerator.GenerateCommands(gameState);
-           var updatedGameState = commands.First()(gameState.BoardState, PieceColour.Black);
-           var updatedBoard = updatedGameState.BoardState.Board;
-           Assert.That(updatedBoard[2,1].CurrentPiece.Type, Is.EqualTo(PieceType.BlackPawn));
-           
-           
+            var commands = _aiMoveCommandGenerator.GenerateCommands(gameState);
+            var updatedGameState = commands.First()(gameState.BoardState, PieceColour.Black);
+            var updatedBoard = updatedGameState.BoardState.Board;
+            Assert.That(updatedBoard[2, 1].CurrentPiece.Type, Is.EqualTo(PieceType.BlackPawn));
+        }
 
+        [Test]
+        public void GeneratesCommands_WithCorrectBuildMoveOutput()
+        {
+            var board = _boardGenerator.GenerateBoard();
+            board[2, 2].CurrentPiece = new Piece(PieceType.BlackPawn);
+            var boardState = new BoardState(board);
+
+            var moves = new Dictionary<Position, ImmutableHashSet<Position>>()
+                .ToImmutableDictionary();
+
+            var buildMoves = new BuildMoves(
+                new HashSet<Position> {new Position(5, 5)}.ToImmutableHashSet(),
+                new HashSet<PieceType> {PieceType.WhitePawn}.ToImmutableHashSet()
+            );
+
+            var gameState = new GameState(
+                false,
+                false,
+                new PlayerState(10),
+                new PlayerState(10),
+                moves,
+                buildMoves,
+                boardState
+            );
+
+            var commands = _aiMoveCommandGenerator.GenerateCommands(gameState);
+            var updatedGameState = commands.First()(gameState.BoardState, PieceColour.White);
+            var updatedBoard = updatedGameState.BoardState.Board;
+            Assert.That(updatedBoard[5,5].BuildTileState.BuildingPiece, Is.EqualTo(PieceType.WhitePawn));
+            Assert.That(updatedBoard[5,5].BuildTileState.Turns, Is.EqualTo(1));
         }
     }
 }

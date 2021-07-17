b2729b21e99d9ac05aabbc3749d8768718dccc42
Harry Prior <harryprior@hotmail.co.uk>
2021-07-17 14:54:42 +0100

    Rename variables

--- Assets/Scripts/Models/Services/Moves/Utils/BoardInfo.cs
+++ Assets/Scripts/Models/Services/Moves/Utils/BoardInfo.cs
@@ -18,18 +18,17 @@ namespace Models.Services.Moves.Utils
         public IDictionary<Position, HashSet<Position>> EnemyMoves { get; private set; }
         public Position KingPosition { get; private set; } = new Position(8, 8);
 
-        // TODO refactor
         public void EvaluateBoard(BoardState boardState, PieceColour turn)
         {
             var board = boardState.Board;
             var turnMoves = new Dictionary<Position, HashSet<Position>>();
-            var nonTurnMoves = new Dictionary<Position, HashSet<Position>>();
+            var enemyMoves = new Dictionary<Position, HashSet<Position>>();
             foreach (var tile in board)
             {
                 var currentPiece = tile.CurrentPiece;
-                var piecesTurn = currentPiece.Type != PieceType.NullPiece && currentPiece.Colour == turn;
-                var notPiecesTurn = currentPiece.Type != PieceType.NullPiece && currentPiece.Colour != turn;
-                if (piecesTurn)
+                var playerTurn = currentPiece.Type != PieceType.NullPiece && currentPiece.Colour == turn;
+                var opponentTurn = currentPiece.Type != PieceType.NullPiece && currentPiece.Colour != turn;
+                if (playerTurn)
                 {
                     if (currentPiece.Type == PieceType.BlackKing || currentPiece.Type == PieceType.WhiteKing)
                         KingPosition = tile.Position;
@@ -40,16 +39,16 @@ namespace Models.Services.Moves.Utils
                     turnMoves.Add(boardPos, new HashSet<Position>(possibleMoves));
                 }
 
-                if (notPiecesTurn)
+                if (opponentTurn)
                 {
                     var boardPos = tile.Position;
                     var possibleMoves = _movesGeneratorRepository.GetPossibleMoveGenerator(currentPiece, false)
                         .GetPossiblePieceMoves(boardPos, boardState);
-                    nonTurnMoves.Add(boardPos, new HashSet<Position>(possibleMoves));
+                    enemyMoves.Add(boardPos, new HashSet<Position>(possibleMoves));
                 }
 
                 TurnMoves = turnMoves;
-                EnemyMoves = nonTurnMoves;
+                EnemyMoves = enemyMoves;
             }
         }
     }

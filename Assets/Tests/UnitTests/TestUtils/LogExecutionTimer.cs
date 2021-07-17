3a078dd2aa7af4cdf01bb13ade8cfe2d31b0a73a
Harry Prior <harryprior@hotmail.co.uk>
2021-07-17 14:28:36 +0100

    Add execution time logger

---
+++ Assets/Tests/UnitTests/TestUtils/LogExecutionTimer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Tests.TestUtils
+{
+    public class LogExecutionTimer
+    {
+        public void LogExecutionTime(string executingFunction, Action function)
+        {
+            var startTime = DateTime.Now.ToString("[yyyy-mm-dd hh-mm-ss]");
+            using var sr = File.CreateText(Path.Combine("Assets/", "Logs/",
+                $"{startTime} Execution time of {executingFunction}.txt"));
+            // path contains illegal chars
+
+
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            function();
+            stopWatch.Stop();
+
+            sr.Write(
+                $"Execution time of {executingFunction}: \n      {stopWatch.ElapsedMilliseconds} ms \n      {stopWatch.ElapsedMilliseconds / 1000} s");
+        }
+    }
+}

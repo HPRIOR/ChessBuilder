c8f87c14464ba86863fb758729feeb94d586128e
Harry Prior <harryprior@hotmail.co.uk>
2021-07-18 17:36:10 +0100

    Move non turn moves into own dir

--- Assets/Scripts/Models/Services/Moves/Factories/MovesFactory.cs
+++ Assets/Scripts/Models/Services/Moves/Factories/MovesFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Models.Services.Interfaces;
+using Models.Services.Moves.MoveGenerators.NonTurnMoves;
 using Models.Services.Moves.MoveGenerators.TurnMoves;
 using Models.State.PieceState;
 

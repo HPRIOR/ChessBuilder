3aab6d5679bee4671f9f5bb97d1d21a315a242a8
Harry Prior <harryprior@hotmail.co.uk>
2021-07-18 18:39:31 +0100

    optimise queen moves

--- Assets/Scripts/Models/Services/Moves/MoveGenerators/TurnMoves/QueenMoves.cs
+++ Assets/Scripts/Models/Services/Moves/MoveGenerators/TurnMoves/QueenMoves.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Models.Services.Interfaces;
 using Models.Services.Moves.Utils;
@@ -25,12 +24,26 @@ namespace Models.Services.Moves.MoveGenerators.TurnMoves
         {
             var relativePosition = _positionTranslator.GetRelativePosition(originPosition);
 
-            return Enum
-                .GetValues(typeof(Direction))
-                .Cast<Direction>()
-                .ToList()
-                .SelectMany(direction => _boardScanner.ScanIn(direction, relativePosition, boardState));
+            var result = GetMovesIn(Direction.N, relativePosition, boardState).ToList();
+            var southMoves = GetMovesIn(Direction.S, relativePosition, boardState).ToList();
+            var eastMoves = GetMovesIn(Direction.E, relativePosition, boardState).ToList();
+            var westMoves = GetMovesIn(Direction.W, relativePosition, boardState).ToList();
+            var northWestMoves = GetMovesIn(Direction.NW, relativePosition, boardState).ToList();
+            var northEastMoves = GetMovesIn(Direction.NE, relativePosition, boardState).ToList();
+            var southWestMoves = GetMovesIn(Direction.SW, relativePosition, boardState).ToList();
+            var southEastMoves = GetMovesIn(Direction.SE, relativePosition, boardState).ToList();
+            result.AddRange(southMoves);
+            result.AddRange(eastMoves);
+            result.AddRange(westMoves);
+            result.AddRange(northWestMoves);
+            result.AddRange(northEastMoves);
+            result.AddRange(southWestMoves);
+            result.AddRange(southEastMoves);
+            return result;
         }
+
+        private IEnumerable<Position> GetMovesIn(Direction d, Position relativePosition, BoardState boardState) =>
+            _boardScanner.ScanIn(d, relativePosition, boardState);
 
         public class Factory : PlaceholderFactory<PieceColour, QueenMoves>
         {

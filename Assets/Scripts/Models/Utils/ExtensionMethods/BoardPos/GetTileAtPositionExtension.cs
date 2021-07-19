384315424ce16af43b298b7ba580a0283c1eaadd
Harry Prior <harryprior@hotmail.co.uk>
2021-07-19 01:31:13 +0100

    Add GetTileAt ext

---
+++ Assets/Scripts/Models/Utils/ExtensionMethods/BoardPos/GetTileAtPositionExtension.cs
@@ -0,0 +1,10 @@
+﻿using Models.State.Board;
+
+namespace Models.Utils.ExtensionMethods.BoardPos
+{
+    public static class GetTileAtPositionExtension
+    {
+        public static Tile TileIn(this Position position, BoardState boardState) =>
+            boardState.Board[position.X, position.Y];
+    }
+}

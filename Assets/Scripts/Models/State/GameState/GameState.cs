75b59c31e71592558121144214f2fa73dbbd05e5
Harry Prior <harryprior@hotmail.co.uk>
2021-08-21 09:37:28 +0100

    Add ability to clone game state
    Signed-off-by: Harry Prior <harryprior@hotmail.co.uk>

--- Assets/Scripts/Models/State/GameState/GameState.cs
+++ Assets/Scripts/Models/State/GameState/GameState.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections.Immutable;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using Models.State.Board;
 using Models.State.BuildState;
+using Models.State.PieceState;
 
 namespace Models.State.GameState
 {
-    public class GameState
+    public class GameState : ICloneable
     {
         public GameState(bool check, bool checkMate, PlayerState.PlayerState blackState,
             PlayerState.PlayerState whiteState,
@@ -29,6 +32,17 @@ namespace Models.State.GameState
         public PlayerState.PlayerState WhiteState { get; set; }
         public ImmutableDictionary<Position, ImmutableHashSet<Position>> PossiblePieceMoves { get; set; }
         public BuildMoves PossibleBuildMoves { get; set; }
+
+        public object Clone()
+        {
+            var possibleMoves = new Dictionary<Position, ImmutableHashSet<Position>>(PossiblePieceMoves)
+                .ToImmutableDictionary();
+            var builds = new HashSet<PieceType>(PossibleBuildMoves.BuildPieces).ToImmutableHashSet();
+            var buildPosition = new HashSet<Position>(PossibleBuildMoves.BuildPositions).ToImmutableHashSet();
+            var buildMoves = new BuildMoves(buildPosition, builds);
+            return new GameState(Check, CheckMate, BlackState, WhiteState, possibleMoves, buildMoves,
+                BoardState.Clone());
+        }
 
         public override string ToString()
         {

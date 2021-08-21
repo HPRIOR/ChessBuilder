2abec03423970f33f70baeb5938475c16b562b4e
Harry Prior <harryprior@hotmail.co.uk>
2021-08-21 09:38:32 +0100

    Remove to string method
    Signed-off-by: Harry Prior <harryprior@hotmail.co.uk>

--- Assets/Scripts/Models/State/GameState/GameState.cs
+++ Assets/Scripts/Models/State/GameState/GameState.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
-using System.Text;
 using Models.State.Board;
 using Models.State.BuildState;
 using Models.State.PieceState;
@@ -42,30 +40,6 @@ namespace Models.State.GameState
             var buildMoves = new BuildMoves(buildPosition, builds);
             return new GameState(Check, CheckMate, BlackState, WhiteState, possibleMoves, buildMoves,
                 BoardState.Clone());
-        }
-
-        public override string ToString()
-        {
-            var stringBuilder = new StringBuilder();
-            stringBuilder.Append("Black state: \n");
-            stringBuilder.Append($"     Build Points: {BlackState.BuildPoints} \n");
-            stringBuilder.Append("White state: \n");
-            stringBuilder.Append($"     Build Points: {WhiteState.BuildPoints} \n");
-            stringBuilder.Append("Possible Moves: \n");
-            PossiblePieceMoves.Keys.ToList().ForEach(piecePosition =>
-            {
-                stringBuilder.Append(
-                    $"     {BoardState.Board[piecePosition.X, piecePosition.Y].CurrentPiece.Type}: \n       ");
-                PossiblePieceMoves[piecePosition].ToList().ForEach(move => stringBuilder.Append($"({move}), "));
-            });
-            stringBuilder.Append("\n");
-            stringBuilder.Append("Possible Build Pieces: \n     ");
-            PossibleBuildMoves.BuildPieces.ToList().ForEach(piece => stringBuilder.Append($"{piece}, "));
-            stringBuilder.Append("\n");
-            stringBuilder.Append("Possible Build Positions: \n     ");
-            PossibleBuildMoves.BuildPositions.OrderBy(p => p.Y).ToList()
-                .ForEach(position => stringBuilder.Append($"({position}), "));
-            return stringBuilder.ToString();
         }
     }
 }

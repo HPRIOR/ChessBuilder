3dc7a0e41ee8693db297374a63ad184b35277118
Harry Prior <harryprior@hotmail.co.uk>
2021-08-22 11:58:47 +0100

    Uncomment ai installer
    Signed-off-by: Harry Prior <harryprior@hotmail.co.uk>

--- Assets/Scripts/Bindings/MonoInstallers/AIInstaller.cs
+++ Assets/Scripts/Bindings/MonoInstallers/AIInstaller.cs
@@ -1,16 +1,15 @@
-// using Bindings.Installers.AIInstallers;
-// using Zenject;
-//
-// namespace Bindings.MonoInstallers
-// {
-//     public class AIInstaller : MonoInstaller
-//     {
-//         public override void InstallBindings()
-//         {
-//             AiPossibleMoveGeneratorInstaller.Install(Container);
-//             AiMoveGeneratorInstaller.Install(Container);
-//             StaticEvaluatorInstaller.Install(Container);
-//         }
-//     }
-// }
+using Bindings.Installers.AIInstallers;
+using Zenject;
 
+namespace Bindings.MonoInstallers
+{
+    public class AIInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            AiPossibleMoveGeneratorInstaller.Install(Container);
+            AiMoveGeneratorInstaller.Install(Container);
+            StaticEvaluatorInstaller.Install(Container);
+        }
+    }
+}

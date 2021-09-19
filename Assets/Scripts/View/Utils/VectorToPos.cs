d75b7ad2a436a11eed516649b9623dc8f338b598
Harry Prior <harryprior@hotmail.co.uk>
2021-09-19 12:25:12 +0100

    Add mechanism for getting vector from position
    Signed-off-by: Harry Prior <harryprior@hotmail.co.uk>

---
+++ Assets/Scripts/View/Utils/VectorToPos.cs
@@ -0,0 +1,10 @@
+﻿using Models.State.Board;
+using UnityEngine;
+
+namespace View.Utils
+{
+    public static class VectorToPos
+    {
+        public static Vector2 GetVector(Position pos) => new Vector2(pos.X + 0.5f, pos.Y + 0.5f);
+    }
+}

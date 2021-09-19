24645ab13dda754d0fcd6a1d3240d7db0a17beba
Harry Prior <harryprior@hotmail.co.uk>
2021-09-19 12:24:40 +0100

    Add switch expression instead of dict
    Signed-off-by: Harry Prior <harryprior@hotmail.co.uk>

--- Assets/Scripts/Models/Services/Moves/Utils/Move.cs
+++ Assets/Scripts/Models/Services/Moves/Utils/Move.cs
@@ -1,23 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using Models.State.Board;
 
 namespace Models.Services.Moves.Utils
 {
     public static class Move
     {
-        private static readonly IDictionary<Direction, Position> DirectionMovement =
-            new Dictionary<Direction, Position>(new DirectionComparer())
-            {
-                { Direction.N, new Position(0, 1) },
-                { Direction.E, new Position(1, 0) },
-                { Direction.S, new Position(0, -1) },
-                { Direction.W, new Position(-1, 0) },
-                { Direction.NE, new Position(1, 1) },
-                { Direction.SE, new Position(1, -1) },
-                { Direction.SW, new Position(-1, -1) },
-                { Direction.NW, new Position(-1, 1) }
-            };
-
-        public static Position In(Direction direction) => DirectionMovement[direction];
+        public static Position In(Direction direction) => direction switch
+        {
+            Direction.N => new Position(0, 1),
+            Direction.NE => new Position(1, 1),
+            Direction.E => new Position(1, 0),
+            Direction.SE => new Position(1, -1),
+            Direction.S => new Position(0, -1),
+            Direction.SW => new Position(-1, -1),
+            Direction.W => new Position(-1, 0),
+            Direction.NW => new Position(-1, 1),
+            _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
+        };
     }
 }

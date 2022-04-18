4dbd2dc1057555a41314d49dd9ceab6fecf2d5b4
HARRY PRIOR <harryprior@hotmail.co.uk>
2022-04-18 11:09:13 +0100

    Add logic for calculating initialisation for all white pieces

--- Assets/Scripts/Models/Services/Game/Implementations/GameInitializer.cs
+++ Assets/Scripts/Models/Services/Game/Implementations/GameInitializer.cs
@@ -1,47 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Models.Services.Build.Interfaces;
 using Models.Services.Moves.Interfaces;
 using Models.State.Board;
 using Models.State.GameState;
 using Models.State.PieceState;
 using Models.State.PlayerState;
+using Models.Utils.ExtensionMethods.PieceTypeExt;
 
 namespace Models.Services.Game.Implementations
 {
     public sealed class GameInitializer
     {
+        private readonly IMovesGeneratorRepository _movesGeneratorRepository;
         private readonly IBuildMoveGenerator _buildMoveGenerator;
-        private readonly IPieceMoveGenerator _whiteKingMoveGenerator;
 
         private GameInitializer(IMovesGeneratorRepository movesGeneratorRepository,
             IBuildMoveGenerator buildMoveGenerator)
         {
+            _movesGeneratorRepository = movesGeneratorRepository;
             _buildMoveGenerator = buildMoveGenerator;
-            _whiteKingMoveGenerator =
-                movesGeneratorRepository.GetPossibleMoveGenerator(PieceType.WhiteKing, true);
         }
+
+        private Dictionary<PieceType, IPieceMoveGenerator> GetPieceMoveGenerators(BoardState boardState) =>
+            boardState.ActivePieces
+                .Select(position => boardState.GetTileAt(position))
+                .Where(tile => tile.CurrentPiece.Colour() == PieceColour.White)
+                .Select(tile => tile.CurrentPiece).Distinct()
+                .ToDictionary(piece => piece,
+                    piece => _movesGeneratorRepository.GetPossibleMoveGenerator(piece, true));
+
+        private List<(Position pos, PieceType type)> GetPiecePositions(BoardState boardState) =>
+            boardState.ActivePieces
+                .Select(position => boardState.GetTileAt(position))
+                .Where(tile => tile.CurrentPiece.Colour() == PieceColour.White)
+                .Select(tile => (position: tile.Position, boardState.GetTileAt(tile.Position).CurrentPiece))
+                .ToList();
 
         public GameState InitialiseGame(BoardState boardState)
         {
-            var whiteKingPosition = GetWhiteKingPosition(boardState);
-            var moves = _whiteKingMoveGenerator.GetPossiblePieceMoves(whiteKingPosition, boardState);
-            var movesDict = new Dictionary<Position, List<Position>>
-            {
-                { whiteKingPosition, new List<Position>(moves) }
-            };
+            var pieceMoveGenerators = GetPieceMoveGenerators(boardState);
+            var piecePositions = GetPiecePositions(boardState);
 
-            var builds = _buildMoveGenerator.GetPossibleBuildMoves(boardState, PieceColour.White, new PlayerState(39));
-            return new GameState(false, false, new PlayerState(39), movesDict, builds, boardState);
-        }
+            var movesDict =
+                piecePositions.ToDictionary(
+                    piecePos => piecePos.pos,
+                    piecePos => pieceMoveGenerators[piecePos.type]
+                        .GetPossiblePieceMoves(piecePos.pos, boardState));
 
-        private Position GetWhiteKingPosition(BoardState boardState)
-        {
-            for (var i = 0; i < 8; i++)
-            for (var j = 0; j < 8; j++)
-                if (boardState.GetTileAt(i, j).CurrentPiece == PieceType.WhiteKing)
-                    return new Position(i, j);
-            throw new Exception("No white king found during game initialisation");
+            var whitePieceValue =
+                piecePositions
+                    .Where(pp => pp.type.Colour() == PieceColour.White)
+                    .Select(pp => pp.type.Value()).Sum();
+
+            // todo inject total build points
+            var buildPoints = 39 - whitePieceValue;
+
+
+            var builds =
+                _buildMoveGenerator.GetPossibleBuildMoves(boardState, PieceColour.White, new PlayerState(buildPoints));
+            return new GameState(false, false, new PlayerState(buildPoints), movesDict, builds, boardState);
         }
     }
 }

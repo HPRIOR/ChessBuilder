a1a02289f2584d743641dd4290b23b1ecf384087
HARRY PRIOR <harryprior@hotmail.co.uk>
2022-05-08 21:55:54 +0100

    Remove DragAndDropNetwork from opposing player

--- Assets/Scripts/View/Prefab/Factories/PieceFactory.cs
+++ Assets/Scripts/View/Prefab/Factories/PieceFactory.cs
@@ -1,15 +1,27 @@
-﻿using Models.Services.Moves.Interfaces;
+﻿using Models.Services.Game.Implementations;
+using Models.Services.Moves.Interfaces;
 using Models.State.Board;
 using Models.State.PieceState;
+using Models.Utils.ExtensionMethods.PieceTypeExt;
+using UnityEngine;
+using View.NetworkUserInput;
 using View.Prefab.Interfaces;
 using View.Prefab.Spawners;
 using View.Utils;
+using Zenject;
 
 namespace View.Prefab.Factories
 {
     public class PieceFactory : IPieceFactory
     {
         private readonly PieceSpawner.Factory _pieceFactory;
+        private GameContext _context;
+
+        [Inject]
+        public void Construct(GameContext context)
+        {
+            _context = context;
+        }
 
         public PieceFactory(PieceSpawner.Factory pieceFactory)
         {
@@ -19,6 +31,10 @@ namespace View.Prefab.Factories
         public IPieceSpawner CreatePiece(PieceType pieceType, Position position)
         {
             var piece = _pieceFactory.Create(new PieceRenderInfo(pieceType), position);
+            if (piece.RenderInfo.PieceType.Colour() != _context.PlayerColour)
+            {
+                Object.Destroy(piece.gameObject.GetComponent<DragAndDropNetwork>());
+            }
             return piece;
         }
     }

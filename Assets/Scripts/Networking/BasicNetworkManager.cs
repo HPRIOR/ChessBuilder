ae0753b9fd77d16745a8776accb30dda67980ff2
HARRY PRIOR <harryprior@hotmail.co.uk>
2022-05-08 21:52:52 +0100

    Manually instantiate player to add rudimentary player selection logic

--- Assets/Scripts/Networking/BasicNetworkManager.cs
+++ Assets/Scripts/Networking/BasicNetworkManager.cs
@@ -1,8 +1,19 @@
 using Mirror;
+using Models.State.PieceState;
 
 namespace Networking
 {
     public class BasicNetworkManager : NetworkManager
     {
+        public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+        {
+            var playerGameObject = Instantiate(playerPrefab);
+            NetworkServer.AddPlayerForConnection(conn, playerGameObject);
+            var playerColour = numPlayers == 1 ? PieceColour.White : PieceColour.Black;
+
+            var player = playerGameObject.GetComponent<Player>();
+            player.SetPlayerContext(playerColour);
+            player.SetGameReady();
+        }
     }
 }
